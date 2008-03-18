324da084b649cea2df056faf355c31dab636b3e7
Frank Hellenkamp <jonas@depagecms.net>
2008-03-18 22:47:09 +0100

    added function to compare sha1 of file to the db-value

--- www/framework/lib/lib_publish.php
+++ www/framework/lib/lib_publish.php
@@ -32,16 +32,22 @@ class publish {
     function get_changed_lib_files() {
         global $conf, $project;
 
+        $changed_files = array();
+
         $ppath = $project->get_project_path($this->project_name);
+
         chdir($ppath . '/lib/');
         $this->_get_changed_lib_files_dir();
         chdir($conf->path_server_root . $conf->path_base . '/framework');
         
         foreach ($this->files as $file) {
             $file->sha1 = sha1_file($ppath . $file->get_fullname());
-            echo($file->sha1 . " " . $file->get_fullname() . "\n");
-            $this->add_file_to_db($file->path, $file->filename, $file->sha1);
+            if ($this->file_changed($file)) {
+                $changed_files[] = $file;
+            }
         }
+
+        return $changed_files;
     }
     /* }}} */
     /* {{{ _get_changed_lib_files_dir */
@@ -57,18 +63,51 @@ class publish {
     }
     /* }}} */
     /* {{{ add_file_to_db */
-    function add_file_to_db($path, $filename, $sha1) {
+    function add_file_to_db($file) {
         global $conf;
 
+        // remove old entry
+        $result = db_query(
+            "DELETE FROM $conf->db_table_publish_files 
+            WHERE 
+                pid={$this->publish_id} AND
+                path='" . mysql_escape_string($file->path) . "' AND
+                filename='" . mysql_escape_string($file->filename) . "'
+            "
+        );
+
+        // add new entry
         $result = db_query(
             "INSERT INTO $conf->db_table_publish_files 
             SET 
                 pid={$this->publish_id}, 
-                path='" . mysql_escape_string($path) . "',
-                filename='" . mysql_escape_string($filename) . "',
-                sha1='$sha1'
+                path='" . mysql_escape_string($file->path) . "',
+                filename='" . mysql_escape_string($file->filename) . "',
+                sha1='{$file->sha1}'
             "
         );
+    }
+    /* }}} */
+    /* {{{ file_changed */
+    function file_changed($file) {
+        global $conf;
+
+        $result = db_query(
+            "SELECT sha1 FROM $conf->db_table_publish_files 
+            WHERE 
+                pid={$this->publish_id} AND
+                path='" . mysql_escape_string($file->path) . "' AND
+                filename='" . mysql_escape_string($file->filename) . "'
+            "
+        );
+    
+        if ($result && mysql_num_rows($result) > 0) {
+            $row = mysql_fetch_assoc($result);
+            if ($row['sha1'] == $file->sha1) {
+                return false;
+            } 
+        } 
+        return true;
     }
     /* }}} */
 }
