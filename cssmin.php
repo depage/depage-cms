597eae3f37926b4685cfe22dbbbb0763e27f4259
joe.scylla <joe.scylla@2273e29d-6945-0410-8e99-033b4a520b20>
2008-08-07 10:14:00 +0000

    ^ Complete rewrite of the minify process # Fixed issue #1: Regular expression are erroneous + Support minify of Patterns like: E > F, E + F, E[attrib="value"], E[lang|="en"] + Option to preserve url()-expressions like: background: url(url/to/image.ext)
    git-svn-id: http://cssmin.googlecode.com/svn/trunk@3 2273e29d-6945-0410-8e99-033b4a520b20

--- cssmin.php
+++ cssmin.php
@@ -20,28 +20,65 @@
  * @author 		Joe Scylla <joe.scylla@gmail.com>
  * @copyright 	2008 Joe Scylla <joe.scylla@gmail.com>
  * @license 	http://opensource.org/licenses/mit-license.php MIT License
- * @version 	1.0 (2008-01-31)
+ * @version 	1.0.1.b1 (2008-08-07)
  */
 class cssmin
 	{
 	/**
 	 * Minifies stylesheet definitions
 	 *
-	 * @param 	string	$v	Stylesheet definitions as string
-	 * @return 	string		Minified stylesheet definitions
+	 * @param	string			$css		Stylesheet definitions as string
+	 * @param	string|array	$options	Array or comma speperated list of options:
+	 * 										- preserve-urls: Preserves every url defined in an url()-
+	 * 										expression. This option is only required if you have 
+	 * 										defined really uncommon urls with multiple spaces or 
+	 * 										combination of colon, semi-colon, plus sign, etc. with 
+	 * 										leading or following spaces.
+	 * @return	string			Minified stylesheet definitions
 	 */
-	public static function minify($v) 
+	public static function minify($css, $options = "")
 		{
-		$v = trim($v);
-		$v = str_replace("\r\n", "\n", $v);
-        $search = array("/\/\*[\d\D]*?\*\/|\t+/", "/\s+/", "/\}\s+/");
-        $replace = array(null, " ", "}\n");
-		$v = preg_replace($search, $replace, $v);
-		$search = array("/\\;\s/", "/\s+\{\\s+/", "/\\:\s+\\#/", "/,\s+/i", "/\\:\s+\\\'/i", "/\\:\s+([0-9]+|[A-F]+)/i");
-        $replace = array(";", "{", ":#", ",", ":\'", ":$1");
-        $v = preg_replace($search, $replace, $v);
-        $v = str_replace("\n", null, $v);
-    	return $v;	
-  		}
+		$options = ($options == "") ? array() : (is_array($options) ? $options : explode(",", $options));
+		if (in_array("preserve-urls", $options))
+			{
+			// Encode url() to base64
+			$css = preg_replace_callback("/url\s*\((.*)\)/siU", array(self, "_encodeUrl"), $css);
+			}
+		// Remove comments
+		$css = preg_replace("/\/\*[\d\D]*?\*\/|\t+/", " ", $css);
+		// Replace CR, LF and TAB to spaces
+		$css = str_replace(array("\n", "\r", "\t"), " ", $css);
+		// Replace multiple to single space
+		$css = preg_replace("/\s\s+/", " ", $css);
+		// Remove unneeded spaces
+		$css = preg_replace("/\s*({|}|\[|\]|=|~|\+|>|\||;|:|,)\s*/", "$1", $css);
+		$css = trim($css);
+		if (in_array("preserve-urls", $options))
+			{
+			// Decode url()
+			$css = preg_replace_callback("/url\s*\((.*)\)/siU", array(self, "_decodeUrl"), $css);
+			}
+		return $css;
+		}
+	/**
+	 * Encodes a url() expression.
+	 *
+	 * @param	array	$match
+	 * @return	string
+	 */
+	private function _encodeUrl($match)
+		{
+		return "url(" . base64_encode(trim($match[1])) . ")";
+		}
+	/**
+	 * Decodes a url() expression.
+	 *
+	 * @param	array	$match
+	 * @return	string
+	 */
+	private function _decodeUrl($match)
+		{
+		return "url(" . base64_decode($match[1]) . ")";
+		}
 	}
 ?>
