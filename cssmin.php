b7aad362020695f1f839c5823104cde43d76ba93
joe.scylla <joe.scylla@2273e29d-6945-0410-8e99-033b4a520b20>
2008-08-15 07:14:20 +0000

    Version 1.0.1.b2  + New method cssmin::toArray() - convert a stylesheet definition to a array structure  + New method cssmin::toString() - convert a array structure to a stylesheet definition  + New helper functions cssmin_array_clean() and cssmin_array_is_assoc().  ^ Moved private functions to helper functions (now named cssmin_decode_url() and cssmin_encode_url())
    git-svn-id: http://cssmin.googlecode.com/svn/trunk@4 2273e29d-6945-0410-8e99-033b4a520b20

--- cssmin.php
+++ cssmin.php
@@ -20,20 +20,24 @@
  * @author 		Joe Scylla <joe.scylla@gmail.com>
  * @copyright 	2008 Joe Scylla <joe.scylla@gmail.com>
  * @license 	http://opensource.org/licenses/mit-license.php MIT License
- * @version 	1.0.1.b1 (2008-08-07)
+ * @version 	1.0.1.b2 (2008-08-15)
  */
 class cssmin
 	{
 	/**
 	 * Minifies stylesheet definitions
 	 *
+	 * <code>
+	 * $css_minified = cssmin::minify(file_get_contents("path/to/target/file.css"));
+	 * </code>
+	 * 
 	 * @param	string			$css		Stylesheet definitions as string
-	 * @param	string|array	$options	Array or comma speperated list of options:
+	 * @param	array|string	$options	Array or comma speperated list of options:
 	 * 										- preserve-urls: Preserves every url defined in an url()-
 	 * 										expression. This option is only required if you have 
 	 * 										defined really uncommon urls with multiple spaces or 
-	 * 										combination of colon, semi-colon, plus sign, etc. with 
-	 * 										leading or following spaces.
+	 * 										combination of colon, semi-colon, braces with leading or 
+	 * 										following spaces.
 	 * @return	string			Minified stylesheet definitions
 	 */
 	public static function minify($css, $options = "")
@@ -42,7 +46,7 @@ class cssmin
 		if (in_array("preserve-urls", $options))
 			{
 			// Encode url() to base64
-			$css = preg_replace_callback("/url\s*\((.*)\)/siU", array(self, "_encodeUrl"), $css);
+			$css = preg_replace_callback("/url\s*\((.*)\)/siU", "cssmin_encode_url", $css);
 			}
 		// Remove comments
 		$css = preg_replace("/\/\*[\d\D]*?\*\/|\t+/", " ", $css);
@@ -56,29 +60,138 @@ class cssmin
 		if (in_array("preserve-urls", $options))
 			{
 			// Decode url()
-			$css = preg_replace_callback("/url\s*\((.*)\)/siU", array(self, "_decodeUrl"), $css);
+			$css = preg_replace_callback("/url\s*\((.*)\)/siU", "cssmin_encode_url", $css);
 			}
 		return $css;
 		}
 	/**
-	 * Encodes a url() expression.
+	 * Return a array structure of a stylesheet definitions.
 	 *
-	 * @param	array	$match
-	 * @return	string
+	 * <code>
+	 * $css_structure = cssmin::toArray(file_get_contents("path/to/target/file.css"));
+	 * </code>
+	 * 
+	 * @param	string		$css			Stylesheet definitions as string
+	 * @param	string		$options		Options for {@link cssmin::minify()}
+	 * @return	array						Structure of the stylesheet definitions as array
 	 */
-	private function _encodeUrl($match)
+	public static function toArray($css, $options = "")
 		{
-		return "url(" . base64_encode(trim($match[1])) . ")";
+		$r = array();
+		$css = cssmin::minify($css, $options);
+		preg_match_all("/(.+){(.+:.+);}/U", $css, $items);
+		if (count($items[0]) > 0)
+			{
+			for ($i = 0; $i < $c = count($items[0]); $i++)
+				{
+				$keys		= explode(",", $items[1][$i]);
+				$styles_tmp	= explode(";", $items[2][$i]);
+				$styles = array();
+				foreach ($styles_tmp as $style)
+					{
+					$style_tmp = explode(":", $style);
+					$styles[$style_tmp[0]] = $style_tmp[1];
+					}
+				$r[] = array
+					(
+					"keys"		=> cssmin_array_clean($keys),
+					"styles"	=> cssmin_array_clean($styles)
+					);
+				}
+			}
+		return $r;
 		}
 	/**
-	 * Decodes a url() expression.
+	 * Return a array structure created by {@link cssmin::toArray()} to a string.
 	 *
-	 * @param	array	$match
-	 * @return	string
+	 * <code>
+	 * $css_string = cssmin::toString($css_structure);
+	 * </code>
+	 * 
+	 * @param	array		$css
+	 * @return	array
 	 */
-	private function _decodeUrl($match)
+	public static function toString(array $array)
 		{
-		return "url(" . base64_decode($match[1]) . ")";
+		$r = "";
+		foreach ($array as $item)
+			{
+			$r .= implode(",", $item["keys"]) . "{";
+			foreach ($item["styles"] as $key => $value)
+				{
+				$r .= $key . ":" . $value . ";";
+				}
+			$r .= "}";
+			}
+		return $r;
 		}
+	}
+
+/**
+ * Trims all elements of the array and removes empty elements. 
+ *
+ * @param	array		$array
+ * @return	array
+ */
+function cssmin_array_clean(array $array)
+	{
+	$r = array();
+	$c = count($v);
+	if (cssmin_array_is_assoc($array))
+		{
+		foreach ($array as $key => $value)
+			{
+			$r[$key] = trim($value);
+			}
+		}
+	else
+		{
+		foreach ($array as $value)
+			{
+			if (trim($value) != "")
+				{
+				$r[] = trim($value);
+				}
+			}
+		}
+	return $r;
+	}
+/**
+ * Return if value is a associative array.
+ *
+ * @param	array		$array
+ * @return	bool
+ */
+function cssmin_array_is_assoc($array)
+	{
+	if (!is_array($array))
+		{
+		return false;
+		}
+	else
+		{
+		krsort($array, SORT_STRING);
+		return !is_numeric(key($array));
+		}
+	}
+/**
+ * Encodes a url() expression.
+ *
+ * @param	array	$match
+ * @return	string
+ */
+function cssmin_encode_url($match)
+	{
+	return "url(" . base64_encode(trim($match[1])) . ")";
+	}
+/**
+ * Decodes a url() expression.
+ *
+ * @param	array	$match
+ * @return	string
+ */
+function cssmin_decode_url($match)
+	{
+	return "url(" . base64_decode($match[1]) . ")";
 	}
 ?>
