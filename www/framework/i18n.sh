3239360715014dfb0f5fbfe77721b3c3637cd54c
Lion Vollnhals <lion.vollnhals@googlemail.com>
2011-02-04 14:11:03 +0100

    translation helper script

---
+++ www/framework/i18n.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+languages=`ls locale | sed -e 's/en_US//' | sed -e 's/CVS//'`
+echo "extract strings..."
+tempfile=`tempfile`
+find ./ -name "*.tpl" -or -name "*.php" > $tempfile
+xgettext -f $tempfile -L PHP -o locale/en_US/LC_MESSAGES/messages_tmp.po
+cat locale/en_US/LC_MESSAGES/messages_tmp.po | sed -e 's/CHARSET/UTF-8/' > locale/en_US/LC_MESSAGES/messages.po
+rm locale/en_US/LC_MESSAGES/messages_tmp.po
+echo "update languages..."
+for lang in $languages
+do
+echo "update $lang..."
+if [[ ! -a locale/$lang/LC_MESSAGES/messages.po ]]
+then
+echo "copy template..."
+cp locale/en_US/LC_MESSAGES/messages.po locale/$lang/LC_MESSAGES/messages.po
+fi
+mv locale/$lang/LC_MESSAGES/messages.po locale/$lang/LC_MESSAGES/messages_old.po
+msgmerge locale/$lang/LC_MESSAGES/messages_old.po locale/en_US/LC_MESSAGES/messages.po \
+-o locale/$lang/LC_MESSAGES/messages.po
+rm locale/$lang/LC_MESSAGES/messages_old.po
+msgfmt -o locale/$lang/LC_MESSAGES/messages.mo locale/$lang/LC_MESSAGES/messages.po
+done
+echo "done!"
+
