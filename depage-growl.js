7209e8a85fefe3aac81c2898c071cf6f27d8f83a
Frank Hellenkamp <jonas@depage.net>
2011-06-19 18:23:30 +0200

    depage-jquery-plugins: added plugins
    - added depage-base to initialize depage-"namespace" 
    - added textbutton-replacer

--- depage-growl.js
+++ depage-growl.js
@@ -10,42 +10,43 @@
  */
 (function( $ ){
 // add depage "namespace" to jquery
-$.extend({ depage: {} });
+    $.extend($.depage, {
+        /* {{{ growl */
+        /**
+         * @function growl()
+         *
+         * shows a growl-like notification 
+         *
+         * @param title     title of the notification
+         * @param message   bodytext of the notification (optional)
+         * @param icons     icon of the notification (optional)
+         */
+        growl: function (title, message, icon) {
+            // get or add notification-area
+            var $notificationArea = $("#depageGrowlArea");
+            if ($notificationArea.length == 0) {
+                $notificationArea = $("<div id=\"depageGrowlArea\"></div>").appendTo("body");
+            }
 
-$.extend($.depage, {
-    /* {{{ growl */
-    /**
-     * @function growl()
-     *
-     * shows a growl-like notification 
-     *
-     * @param title     title of the notification
-     * @param message   bodytext of the notification (optional)
-     * @param icons     icon of the notification (optional)
-     */
-    growl: function (title, message, icon) {
-        // get or add notification-area
-        var $notificationArea = $("#depageGrowlArea");
-        if ($notificationArea.length == 0) {
-            $notificationArea = $("<div id=\"depageGrowlArea\"></div>").appendTo("body");
+            title = title || "";
+            message = message || "";
+            icon = icon || "";
+
+
+            var $notification = $("<div><h3>" + title + "</h3><p>" + message + "</p></div>")
+                .appendTo($notificationArea)
+                .click( function() {
+                    $(this).remove();
+                })
+                .hide()
+                .fadeIn(400)
+                .animate( { opacity: 1 }, 3500)
+                .fadeOut(800, function() { 
+                    $(this).remove();
+                });
         }
-
-        title = title || "";
-        message = message || "";
-        icon = icon || "";
-
-
-        var $notification = $("<div><h3>" + title + "</h3><p>" + message + "</p></div>")
-            .appendTo($notificationArea)
-            .click( function() {
-                $(this).remove();
-            })
-            .animate( { opacity: 1 }, 3500).fadeOut(800, function() { 
-                $(this).remove();
-            });
-    },
-    /* }}} */
-})
+        /* }}} */
+    })
 })( jQuery );
 
 /* vim:set ft=javascript sw=4 sts=4 fdm=marker : */
