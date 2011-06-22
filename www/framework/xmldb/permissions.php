4c4996803867400b9605c3c8837313709b572639
Lion Vollnhals <lion.vollnhals@googlemail.com>
2011-06-22 16:39:51 +0200

    allow wildcard in permissions and generate default element in valid_children()

--- www/framework/xmldb/permissions.php
+++ www/framework/xmldb/permissions.php
@@ -14,6 +14,9 @@
 namespace depage\xmldb; 
 
 class permissions {
+    const wildcard = "all";
+    const default_element = "default";
+
     protected $allow_element_in = array();
     protected $allow_unlink_of = array();
 
@@ -29,18 +32,27 @@ class permissions {
         }
 
         if (!in_array($target, $this->allow_element_in[$element])) {
-            $this->allow_element_in[$element][] = $target;
+            if ($target !== self::wildcard) {
+                $this->allow_element_in[$element][] = $target;
+            } else {
+                $this->allow_element_in[$element] = array($target);
+            }
         }
     }
 
     public function allow_unlink_of($element) {
         if (!in_array($element, $this->allow_unlink_of)) {
-            $this->allow_unlink_of[] = $element;
+            if ($element !== self::wildcard) {
+                $this->allow_unlink_of[] = $element;
+            } else {
+                $this->allow_unlink_of[] = array($element);
+            }
         }
     }
 
     public function is_element_allowed_in($element, $target) {
-        return isset($this->allow_element_in[$element]) && in_array($target, $this->allow_element_in[$element]);
+        return isset($this->allow_element_in[$element]) && 
+            (in_array($target, $this->allow_element_in[$element]) || in_array(self::wildcard, $this->allow_element_in[$element]));
     }
 
     public function is_element_allowed_in_any($element) {
@@ -48,7 +60,7 @@ class permissions {
     }
 
     public function is_unlink_allowed_of($element) {
-        return in_array($element, $this->allow_unlink_of);
+        return in_array($element, $this->allow_unlink_of) || in_array(self::wildcard, $this->allow_unlink_of);
     }
 
     public function valid_children() {
@@ -62,6 +74,20 @@ class permissions {
 
                 $valid_children_for[$target][] = $element;
             }
+        }
+
+        if (!isset($valid_children_for[self::default_element])) {
+            $valid_children_for[self::default_element] = array();
+        }
+
+        // resolve wildcard
+        if (isset($valid_children_for[self::wildcard])) {
+            $known_elements = array_unique(array_keys($valid_children_for), array_keys($this->allow_element_in));
+            foreach ($known_elements as $element) {
+                $valid_children_for[$element] = array_unique($valid_children_for[$element] + $valid_children_for[self::wildcard]);
+            }
+
+            unset($valid_children_for[self::wildcard]);
         }
 
         return $valid_children_for;
