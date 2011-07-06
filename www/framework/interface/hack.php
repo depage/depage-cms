d6ecfb41d42a4f6edb1c6907cc5bb0c3cb773f5d
Frank Hellenkamp <jonas@depage.net>
2011-07-06 22:59:41 +0200

    removed old/unused files from 1.0/1.1-version

--- www/framework/interface/hack.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once('../lib/lib_global.php');
-	require_once('lib_html.php');
-	
-	die_error("you are not allowed to to this!");
-?>
--- www/framework/sql/dp_newproject_xmldata_cache.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `dp_newproject_xmldata_cache` (
-  `id` int(10) unsigned NOT NULL DEFAULT '0',
-  `nodefunc` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
-  `value` mediumtext CHARACTER SET latin1,
-  KEY `SECONDARY` (`nodefunc`,`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='depage::cms 1.1';
