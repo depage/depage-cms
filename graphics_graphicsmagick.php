d25f43c76348fe4ccd66bab67d21238d22b5a7a7
Sebastian Reinhold <sebastian@bitbernd.de>
2011-09-08 12:51:06 +0200

    graphics: fixed graphicsmagic background bug
    fixed checkerboard background with hack

--- graphics_graphicsmagick.php
+++ graphics_graphicsmagick.php
@@ -21,13 +21,24 @@ class graphics_graphicsmagick extends graphics_imagemagick {
 
         $this->processQueue();
 
-        $this->background();
-
-        $this->command .= " {$this->output}";
+        $this->command .= " {$this->output}.miff";
 
         exec($this->command . ' 2>&1', $commandOutput, $returnStatus);
         if ($returnStatus != 0) {
             throw new graphicsException(implode("\n", $commandOutput));
         }
+
+        $this->command = $this->executable . " convert";
+
+        $this->background();
+
+        $this->command .= " -background none {$this->output}.miff -flatten {$this->output}";
+
+        exec($this->command . ' 2>&1', $commandOutput, $returnStatus);
+        if ($returnStatus != 0) {
+            throw new graphicsException(implode("\n", $commandOutput));
+        }
+
+        unlink("{$this->output}.miff");
     }
 }
