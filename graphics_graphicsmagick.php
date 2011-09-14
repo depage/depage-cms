d1c91041eccd43bf959f67a8df891042feee101a
Sebastian Reinhold <sebastian@bitbernd.de>
2011-09-14 13:49:53 +0200

    graphics: fixed graphicsmagick background issues

--- graphics_graphicsmagick.php
+++ graphics_graphicsmagick.php
@@ -2,7 +2,36 @@
 
 namespace depage\graphics;
 
-class graphics_graphicsmagick extends graphics_imagemagick {
+class graphics_graphicsmagick extends graphics {
+    public function __construct($options) {
+        parent::__construct($options);
+
+        $this->executable = $options['executable'];
+    }
+
+    protected function crop($width, $height, $x = 0, $y = 0) {
+        // '+' for positive offset (the '-' is already there)
+        $x = ($x < 0) ? $x : '+' . $x;
+        $y = ($y < 0) ? $y : '+' . $y;
+
+        $xExtent = ($x > 0) ? "+0" : $x;
+        $yExtent = ($y > 0) ? "+0" : $y;
+        $this->command .= " -background none -gravity NorthWest -crop {$width}x{$height}{$x}{$y}\! -gravity NorthWest -extent {$width}x{$height}{$xExtent}{$yExtent}";
+        $this->size = array($width, $height);
+    }
+
+    protected function resize($width, $height) {
+        $newSize = $this->dimensions($width, $height);
+
+        $this->command .= " -background none -resize {$newSize[0]}x{$newSize[1]}\!";
+        $this->size = $newSize;
+    }
+
+    protected function thumb($width, $height) {
+        $this->command .= " -background none -gravity Center -thumbnail {$width}x{$height} -extent {$width}x{$height}";
+        $this->size = array($width, $height);
+    }
+
     public function render($input, $output = null) {
         $this->input        = $input;
         $this->imageSize    = getimagesize($this->input);
@@ -23,9 +52,9 @@ class graphics_graphicsmagick extends graphics_imagemagick {
 
         $this->command = $this->executable . " convert";
 
-        $this->background();
-
-        $this->command .= " -background none {$this->output}.miff -flatten {$this->output}";
+        $this->command .= $this->background();
+        $flatten = ($this->background() == null) ? '' : " -flatten";
+        $this->command .= " -page {$this->size[0]}x{$this->size[1]} {$this->output}.miff{$flatten} {$this->output}";
 
         exec($this->command . ' 2>&1', $commandOutput, $returnStatus);
         if ($returnStatus != 0) {
@@ -33,5 +62,21 @@ class graphics_graphicsmagick extends graphics_imagemagick {
         }
 
         unlink("{$this->output}.miff");
+    }
+
+    protected function background() {
+    $background = " -page {$this->size[0]}x{$this->size[1]} -size {$this->size[0]}x{$this->size[1]}";
+        if ($this->background[0] === '#') {
+            // TODO escape!!
+            $background .= " -background \"{$this->background}\"";
+        } else if ($this->background == 'checkerboard') {
+            $background .= " pattern:checkerboard";
+        } else if ($this->outputFormat == 'jpg') {
+            $background .= " -background \"#FFF\"";
+        } else {
+            $background = null;
+        }
+
+        return $background;
     }
 }
