4206afc497623ab825dc35f6b5799d64f892ff09
Sebastian Reinhold <sebastian@bitbernd.de>
2011-10-05 13:37:36 +0200

    graphics: documented graphics_ui.php

--- www/framework/graphics/graphics_ui.php
+++ www/framework/graphics/graphics_ui.php
@@ -1,20 +1,48 @@
 <?php
+/**
+ * @file    graphics_ui.php
+ * @brief   Interface for accessing graphics via URI
+ *
+ * @author  Frank Hellenkamp <jonas@depage.net>
+ * @author  Sebastian Reinhold <sebastian@bitbernd.de>
+ **/
 
 namespace depage\graphics;
 
 require_once('../depage/depage.php');
 
+/**
+ * @brief Interface for accessing graphics via URI
+ *
+ * Translates GET data to graphics actions.
+ **/
 class graphics_ui {
+    /**
+     * @brief Default options array for graphics factory
+     **/
     public $defaults = array(
         'extension'     => 'gd',
         'background'    => 'transparent',
     );
 
+    /**
+     * @brief graphics_ui class constructor
+     *
+     * @param $options (array) image processing parameters
+     **/
     public function __construct($options = NULL) {
         $conf = new config($options);
         $this->options = $conf->getDefaultsFromClass($this);
     }
 
+    /**
+     * @brief Translates GET data to graphics actions
+     *
+     * Createѕ graphics object and performs action on image. It saves the image
+     * to the cache and displays it.
+     *
+     * @return void
+     **/
     public function convert() {
         $command    = explode('-', $_GET['command']);
         $size       = explode('x', $command[1]);
