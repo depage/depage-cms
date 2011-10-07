316796c7c46ef7afec479fdfe2cec6967d0dad80
Sebastian Reinhold <sebastian@bitbernd.de>
2011-10-07 19:09:02 +0200

    graphics: changed graphics_ui call
    - moved graphics_controller to graphics_ui

--- graphics_ui.php
+++ graphics_ui.php
@@ -1,13 +1,122 @@
 <?php
 /**
  * @file    graphics_ui.php
- * @brief   User interface script to access controller
+ * @brief   Interface for accessing graphics via URI
  *
  * @author  Frank Hellenkamp <jonas@depage.net>
  * @author  Sebastian Reinhold <sebastian@bitbernd.de>
  **/
 
-require_once('../depage/depage.php');
+namespace depage\graphics;
 
-$controller = new graphics_controller();
-$controller->convert();
+/**
+ * @brief Interface for accessing graphics via URI
+ *
+ * Translates request to graphics actions.
+ **/
+class graphics_ui extends \depage_ui {
+    /**
+     * @brief Default options array for graphics factory
+     **/
+    public $defaults = array(
+        'extension'     => 'gd',
+        'background'    => 'transparent',
+    );
+
+    // }}}
+    // {{{ notfound()
+    public function notfound($function = "") {
+        $this->convert($function);
+    }
+    // }}}
+    // {{{ convert()
+    /**
+     * @brief Translates request into graphics actions
+     *
+     * Createѕ graphics object and performs action on image. It saves the image
+     * to the cache and displays it.
+     *
+     * @return void
+     **/
+    private function convert($request) {
+        preg_match('/(.*(gif|jpg|jpeg|png))\.(resize|crop|thumb)-(.*)x(.*)\.(gif|jpg|jpeg|png)/', $request, $command);
+
+        // escape everything
+        $file       = escapeshellcmd($command[1]);
+        $action     = $this->letters($command[3]);
+        $width      = intval($command[4]);
+        $height     = intval($command[5]);
+        $extension  = $this->letters($command[6]);
+
+        $cachedFile = (DEPAGE_CACHE_PATH . "graphics/{$file}.{$action}-{$width}x{$height}.{$extension}");
+
+        $img = graphics::factory(
+            array(
+                'extension'     => $this->defaults['extension'],
+                'background'    => $this->defaults['background'],
+            )
+        );
+
+        $this->mkPathToFile($cachedFile);
+
+        try {
+            $img->{"add$action"}($width, $height)->render($file, $cachedFile);
+        } catch (depage\graphics\graphics_file_not_found_exception $expected) {
+            header("HTTP/1.1 404 Not Found");
+        } catch (depage\graphics\graphics_exception $expected) {
+            header("HTTP/1.1 500 Internal Server Error");
+        }
+
+        $this->display($cachedFile, $extension);
+    }
+    // }}}
+    // {{{ display()
+    /**
+     * @brief Displays image
+     *
+     * @param   $fileName (string) path to image
+     * @param   $format   (string) image format
+     * @return  void
+     **/
+    protected function display($fileName, $format) {
+        if ($format === 'jpg' || $format === 'jpeg') {
+            header("Content-type: image/jpeg");
+            imagejpeg(imagecreatefromjpeg($fileName));
+        } else if ($format === 'png') {
+            header("Content-type: image/png");
+            imagejpeg(imagecreatefrompng($fileName));
+        } else if ($format === 'gif') {
+            header("Content-type: image/gif");
+            imagejpeg(imagecreatefromgif($fileName));
+        }
+    }
+    // }}}
+    // {{{ letters()
+    /**
+     * @brief Cleans up strings
+     *
+     * Removes everything except letters from given string and returns it in
+     * lowercase.
+     *
+     * @return (string) cleaned up string
+     **/
+    protected function letters($string) {
+        return strtolower(preg_replace("[^A-Za-z]", '', $string));
+    }
+    // }}}
+    // {{{ mkPathToFile()
+    /**
+     * @brief Creates path to file
+     *
+     * @param   $file (string) path/file
+     * @return  void
+     **/
+    protected function mkPathToFile($file) {
+        $cachePath = dirname($file);
+        if (!is_dir($cachePath)) {
+            mkdir($cachePath, 0755, true);
+        }
+    }
+    // }}}
+    protected function send_time() {}
+}
