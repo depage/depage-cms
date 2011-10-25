9314ab4837b989a5e8da3d328d28e6cc02c52424
Frank Hellenkamp <jonas@depage.net>
2011-10-25 15:48:20 +0200

    jquery-plugins: added detail-handler for definition lists as plugin

---
+++ www/framework/shared/depage-jquery-plugins/depage-details.js
@@ -0,0 +1,50 @@
+/**
+ * @require framework/shared/jquery-1.4.2.js
+ *
+ * @file    depage-details.js
+ *
+ * adds details handler to definition-lists
+ *
+ *
+ * copyright (c) 2011 Frank Hellenkamp [jonas@depagecms.net]
+ */
+(function( $ ){
+    $.extend($.depage.fnMethods, {
+        /* {{{ details */
+        /**
+         * @function details()
+         *
+         * adds detail handler to definition lists
+         *
+         * @param selector  elements to which the handler will be attached
+         */
+        details: function(selector) {
+            return this.each( function() {
+                $details = this;
+
+                $("dt", this).each(function() {
+                    var $head = $(this).css({
+                        cursor: "pointer"
+                    });
+                    var $detail = $head.nextAll("dd:first");
+
+                    $detail.hide();
+                    $head.click(function() {
+                        if (!$head.hasClass("active")) {
+                            $("dt", $details).removeClass("active");
+                            $head.addClass("active")
+                            $head.siblings("dd").slideUp();
+                            $detail.slideDown();
+                        } else {
+                            $head.removeClass("active")
+                            $detail.slideUp();
+                        }
+                    });
+                });
+            });
+        }
+        /* }}} */
+    });
+})( jQuery );
+
+/* vim:set ft=javascript sw=4 sts=4 fdm=marker : */
