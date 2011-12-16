dd9c185767fa2f2e3357e4ad870c8dcc5a47add2
Frank Hellenkamp <jonas@depage.net>
2011-12-16 17:50:45 +0100

    depage-jquery-plugins: fixed slideshow to hide images under the current image

--- depage-slideshow.js
+++ depage-slideshow.js
@@ -22,16 +22,11 @@
          */
         slideshow: function(selector) {
             return this.each( function() {
-                var divs = $("div, span", this);
-                var speed = Number($(this).attr("data-slideshow-speed"));
-                if (!speed) {
-                    var speed = 3000;
-                }
+                var divs = $(this).children("div, span");
+                var speed = Number($(this).attr("data-slideshow-speed")) || 3000;
 
-                var pause = Number($(this).attr("data-slideshow-pause"));
-                if (!pause) {
-                    var pause = 3000;
-                }
+                var pause = Number($(this).attr("data-slideshow-pause")) || 3000;
+
                 if ($.browser.iphone) {
                     speed = 0;
                     pause = 5000;
@@ -46,24 +41,21 @@
                     $(divs[i]).hide();
                 }
 
+                var last = divs[0];
                 var fadeIn = function(n) {
                     // wait
                     $(divs[n]).animate({top: "0"}, pause, function() {
+                        if (last) {
+                            $(last).fadeOut(speed);
+                        }
                         // fade in
                         $(this).fadeIn(speed, function() {
+                            last = divs[n];
                             if (n < divs.length - 1) {
                                 // fade in next image
                                 fadeIn(n + 1);
                             } else {
-                                // hide all images, fade out last
-                                for (var i = 1; i < divs.length - 1; i++) {
-                                    $(divs[i]).hide();
-                                }
-                                $(divs[n]).animate({top: 0}, pause, function() {
-                                    $(divs[n]).fadeOut(speed, function() {
-                                        fadeIn(1);
-                                    });
-                                });
+                                fadeIn(0);
                             }
                         });
                     });
