fcb8190b36a660cc97b2180453a4314f62a7ee82
Frank Hellenkamp <jonas@depage.net>
2012-03-21 17:14:26 +0100

    restructed task and taskrunner
    - reduced it to only 2 classes
    - added keeping of local variables during run
    - added 2 factory function: load and create

--- www/framework/task/task.php
+++ www/framework/task/task.php
@@ -1,52 +1,88 @@
 <?php
+/**
+ * @file    framework/task/task_runner.php
+ *
+ * depage cms task runner module
+ *
+ *
+ * copyright (c) 2011 Lion Vollnhals [lion.vollnhals@googlemail.com]
+ * copyright (c) 2012 Frank Hellenkamp [jonas@depagecms.net]
+ *
+ * @author    Frank Hellenkamp [jonas@depage.net]
+ * @author    Lion Vollnhals [lion.vollnhals@googlemail.com]
+ */
 
 namespace depage\task;
 
 class task {
     private $tmpvars = array();
 
-    public function __construct($task_id_or_name, $table_prefix, $pdo) {
+    // {{{ constructor
+    private function __construct($table_prefix, $pdo) {
         $this->task_table = $table_prefix . "_tasks";
         $this->subtask_table = $table_prefix . "_subtasks";
         $this->pdo = $pdo;
 
-        if (is_int($task_id_or_name)) {
-            $this->task_id = $task_id_or_name;
-        } else {
-            $this->task_id = $this->create_task($task_id_or_name);
-        }
-
-        $this->lock_name = sys_get_temp_dir() . '/' . $table_prefix . "." . $this->task_id . '.lock';
-
-        $this->load_task();
-        $this->load_subtasks();
     }
+    // }}}
+    
+    // {{{ load()
+    static public function load($task_id, $table_prefix, $pdo) {
+        $task = new task($table_prefix, $pdo);
 
-    public function set_task_status($status) {
+        $task->task_id = $task_id;
+
+        $task->lock_name = sys_get_temp_dir() . '/' . $table_prefix . "." . $task->task_id . '.lock';
+
+        $task->loadTask();
+        $task->loadSubtasks();
+
+        return $task;
+    }
+    // }}}
+    // {{{ create()
+    static public function create($task_name, $table_prefix, $pdo) {
+        $task = new task($table_prefix, $pdo);
+
+        $task->task_id = $task->createTask($task_name);
+        $task->lock_name = sys_get_temp_dir() . '/' . $table_prefix . "." . $task->task_id . '.lock';
+
+        $task->loadTask();
+        $task->loadSubtasks();
+
+        return $task;
+    }
+    // }}}
+    
+    // {{{ setTaskStatus()
+    public function setTaskStatus($status) {
         $query = $this->pdo->prepare("UPDATE {$this->task_table} SET status = :status WHERE id = :id");
         $query->execute(array(
             "status" => $status,
             "id" => $this->task_id,
         ));
     }
-
-    public function set_subtask_status($subtask, $status) {
+    // }}}
+    // {{{ setSubtaskStatus()
+    public function setSubtaskStatus($subtask, $status) {
         $query = $this->pdo->prepare("UPDATE {$this->subtask_table} SET status = :status WHERE id = :id");
         $query->execute(array(
             "status" => $status,
             "id" => $subtask->id,
         ));
     }
-
-    public function get_next_subtask() {
+    // }}}
+    // {{{ getNextSubtask();
+    public function getNextSubtask() {
         $subtask = current($this->subtasks);
         next($this->subtasks);
 
         return $subtask;
     }
-
+    // }}}
+    // {{{ runSubtask()
     /* @return NULL|false returns NULL for no error, false for a parse error */
-    public function run_subtask($subtask) {
+    public function runSubtask($subtask) {
         // readd local variables
         foreach ($this->tmpvars as $_tmpindex => $_tmpvar) {
             $$_tmpindex = $_tmpvar;
@@ -61,25 +97,30 @@ class task {
 
         return $value;
     }
-
+    // }}}
+    
+    // {{{ lock()
     public function lock() {
         $this->lock_file = fopen($this->lock_name, 'w');
         return flock($this->lock_file, LOCK_EX | LOCK_NB);
     }
-
+    // }}}
+    // {{{ unlock()
     public function unlock() {
         flock($this->lock_file, LOCK_UN);
     }
-
-    /* create_subtask only creates task in db.
+    // }}}
+    
+    // {{{ addSubtask()
+    /* addSubtask only creates task in db.
      * the current instance is NOT modified.
      * reload task from the db if you want to execute subtasks.
      *
-     * also see create_subtasks for more convenience.
+     * also see addSubtasks for more convenience.
      *
      * @return int return id of created subtask that can be used for depends_on
      */
-    public function create_subtask($name, $php, $depends_on = NULL) {
+    public function addSubtask($name, $php, $depends_on = NULL) {
         $query = $this->pdo->prepare("INSERT INTO {$this->subtask_table} (task_id, name, php, depends_on) VALUES (:task_id, :name, :php, :depends_on)");
         $query->execute(array(
             "task_id" => $this->task_id,
@@ -90,12 +131,13 @@ class task {
 
         return $this->pdo->lastInsertId();
     }
-
-    /* create_subtasks creates multiple subtasks.
+    // }}}
+    // {{{ addSubtasks()
+    /* addSubtasks creates multiple subtasks.
      * specify tasks as an array of arrays containing name, php and depends_on keys.
      * depends_on references another task in this array by index.
      */
-    public function create_subtasks($tasks) {
+    public function addSubtasks($tasks) {
         foreach ($tasks as &$task) {
             if (!is_array($task)) {
                 throw new \Exception ("malformed task array");
@@ -107,11 +149,14 @@ class task {
                 $depends_on = NULL;
             }
 
-            $task["id"] = $this->create_subtask($task["name"], $task["php"], $depends_on);
+            $task["id"] = $this->addSubtask($task["name"], $task["php"], $depends_on);
         }
     }
-
-    private function create_task($task_name) {
+    // }}}
+    
+    // private functions
+    // {{{ createTask()
+    private function createTask($task_name) {
         $query = $this->pdo->prepare("INSERT INTO {$this->task_table} (name) VALUES (:name)");
         $query->execute(array(
             "name" => $task_name,
@@ -119,23 +164,27 @@ class task {
 
         return $this->pdo->lastInsertId();
     }
-
-    private function load_task() {
+    // }}}
+    // {{{ loadTask();
+    private function loadTask() {
         $query = $this->pdo->prepare("SELECT name, status FROM {$this->task_table} WHERE id = :id");
         $query->execute(array(
             "id" => $this->task_id,
         ));
 
         $result = $query->fetchObject();
-        if (empty($result))
+        if (empty($result)) {
             throw new \Exception("no such task");
-        if (!empty($result->status))
+        }
+        if (!empty($result->status)) {
             throw new \Exception("task was already run.");
+        }
 
         $this->task_name = $result->name;
     }
-
-    private function load_subtasks() {
+    // }}}
+    // {{{ loadSubtasks()
+    private function loadSubtasks() {
         $query = $this->pdo->prepare(
             "SELECT *
             FROM {$this->subtask_table}
@@ -155,20 +204,21 @@ class task {
 
             if (empty($subtask->status)) {
                 $this->subtasks[$subtask->id] = $subtask;
-                $this->include_dependent_subtasks($subtask, $id_to_subtask);
+                $this->includeDependentSubtask($subtask, $id_to_subtask);
             }
         }
 
         ksort($this->subtasks);
     }
-
-    private function include_dependent_subtasks($subtask, &$id_to_subtask) {
+    // }}}
+    // {{{ includeDependentSubtask()
+    private function includeDependentSubtask($subtask, &$id_to_subtask) {
         while ($subtask->depends_on) {
             $subtask = $id_to_subtask[$subtask->depends_on];
             $this->subtasks[$subtask->id] = $subtask;
         }
     }
+    // }}}
 }
-
 
 /* vim:set ft=php sw=4 sts=4 fdm=marker : */
---
+++ www/framework/task/taskrunner.php
@@ -0,0 +1,298 @@
+<?php
+/**
+ * @file    framework/task/task_runner.php
+ *
+ * depage cms task runner module
+ *
+ *
+ * copyright (c) 2011 Lion Vollnhals [lion.vollnhals@googlemail.com]
+ *
+ * @author    Lion Vollnhals [lion.vollnhals@googlemail.com]
+ */
+
+namespace depage\task;
+
+require_once(__DIR__ . "/../depage/depage.php");
+
+/*
+ * task runner design:
+ * 
+ * task_runner#task reads task with respective id from table *_tasks .
+ * task is locked by writing a file lock.
+ * individual subtasks are read from table *_subtasks .
+ * each subtask and task has a status that is updated after running it.
+ * if a subtask fails then its status is set to "failed: {$error_message}",
+ * the corresponding task is also set to "failed".
+ * 
+ * general errors are signaled by throwing exceptions. for example when
+ * there is no such task or when the task is already running.
+ * 
+ * logging is done to logs/depage_task_task_runner.log .
+ * 
+ * php's max execution timeout is anticipated, the script is automatically
+ * restarted and the task will be resumed.
+ * dependent subtasks will also be redone.
+ * if possible the script will be re-executed by php CLI, otherwise the
+ * current request will be repeated.
+ */
+
+/* TODO:
+ webserver timeouts may happen before php max execution timeout:
+ "Your web server can have other timeout configurations that
+ may also interrupt PHP execution. Apache has a Timeout directive
+ and IIS has a CGI timeout function. Both default to 300 seconds.
+ See your web server documentation for specific details."
+
+*/
+
+
+class task_runner extends \depage_ui {
+    // {{{ constructor
+    public function __construct($options = NULL, $cli = false) {
+        parent::__construct($options);
+
+        // overwrite config with real values. TODO: find a better way to do that
+        if ($cli) {
+            $conf = new \config();
+            $conf->readConfig(__DIR__ . "/../../conf/dpconf.php");
+            $this->options = $conf->getFromDefaults($this->defaults);
+            
+            // create log
+            $this->_init();
+        }
+        
+        // get database instance
+        $this->pdo = new \db_pdo (
+            $this->options->db->dsn, // dsn
+            $this->options->db->user, // user
+            $this->options->db->password, // password
+            array(
+                'prefix' => $this->options->db->prefix, // database prefix
+            )
+        );
+
+        $this->prefix = $this->pdo->prefix;
+        
+        $this->force_login = false; // TODO: !$cli;
+        if ($this->force_login) {
+            // get auth object
+            $this->auth = \auth::factory(
+                $this->pdo, // db_pdo 
+                $this->options->auth->realm, // auth realm
+                DEPAGE_BASE, // domain
+                $this->options->auth->method // method
+            );
+        }
+    }
+    // }}}
+    
+    // {{{ run
+    public function run($task_id) {
+        if ($this->force_login)
+            $this->auth->enforce();
+
+        $this->task = task::load((int)$task_id, $this->prefix, $this->pdo);
+        $this->abnormal_exit = true;
+        register_shutdown_function(array($this, "atShutdown"));
+        
+        if ($this->task->lock()) {
+            try {
+                $this->log->log("starting task {$task_id} ({$this->task->task_name})");
+
+                while ($subtask = $this->task->getNextSubtask()) {
+                    $subtask_name = "{$subtask->id} ({$subtask->name})";
+                    $this->log->log("    starting subtask $subtask_name");
+
+                    $status = $this->task->runSubtask($subtask);
+                    if ($status === false) {
+                        throw new \Exception("Parse Error");
+                    }
+                    
+                    $this->log->log("    finished subtask $subtask_name");
+                    $this->task->setSubtaskStatus($subtask, "done");
+                }
+
+                $this->log->log("finished task {$task_id} ({$this->task->task_name})");
+                $this->task->setTaskStatus("done");
+            } catch (\Exception $e) {
+                $this->task->setSubtaskStatus($subtask, "failed: " . $e->getMessage());
+                $this->task->setTaskStatus("failed");
+                $this->log->log("ERROR: " . $e->getMessage());
+            }
+            
+            $this->task->unlock();
+        } else {
+            $this->log->log("task {$this->task->task_name} is already running");
+        }
+
+        $this->abnormal_exit = false;        
+    }
+    // }}}
+    // {{{ runInBackground
+    public function runInBackground($task_id, $lowPriority = false) {
+        $this->lowPriority = $lowPriority;
+        $this->task = task::load((int)$task_id, $this->table_prefix, $this->pdo);
+        $this->abnormal_exit = true;
+
+        register_shutdown_function(array($this, "atShutdown"));
+    }
+    // }}}
+
+    // {{{ atShutdown
+    public function atShutdown() {
+        if ($this->abnormal_exit) {
+            // release last held lock, so that new task can grab it
+            $this->task->unlock();
+
+            $this->executeInBackground(__DIR__ . "/../../", "framework/task/" . basename(__FILE__), $this->task->task_id, $this->lowPriority);
+        } else {
+            $this->log->log("normal exit");
+        }
+    }
+    // }}}
+    
+    // private
+    // {{{ executeInBackground()
+    /**
+    * executes another php script in background
+    *
+    * script is executed as background task
+    * and function returns immediately to current script.
+    *
+    * @public
+    *
+    * @param    $path (string)
+    * @param    $script (string)
+    * @param    $args (string)
+    * @param    $start_low_priority (bool)
+    */
+    private function executeInBackground($path, $script, $args = '', $start_low_priority = false) {
+        $path_phpcli = $this->getPhpExecutable();
+
+        if ($path_phpcli && is_executable($path_phpcli)) {
+            // call script in background through cli executable
+            // this is the finest, because cli scripts has generally no timeout
+            // but unfortunately not available in all cases/platforms
+            if (file_exists($path . $script) || $path == '') {
+                chdir($path);
+                $prio_param = "";
+                if (substr(php_uname(), 0, 7) == 'Windows') {
+                    if ($start_low_priority) {
+                        $prio_param = "/belownormal";
+                    }
+                    pclose(popen("start \"php subTask\" /min $prio_param \"" . str_replace("/", "\\", $path_phpcli) . "\" -f $script " . escapeshellarg($args), "r"));    
+                } else {
+                    if ($start_low_priority) {
+                        $prio_param = "nice -10";
+                    }
+                    pclose(popen("$prio_param \"$path_phpcli\" -f $script -- " . escapeshellarg($args) . " > /dev/null &", "r"));    
+                }
+            }
+        // should only be called if original request was not by cli
+        } else {
+            // call script through http
+            $host = $_SERVER['HTTP_HOST'];
+            if ($host == "") {
+                $host = $_SERVER['SERVER_NAME'];
+            }
+            if ($host == "") {
+                $host = $_SERVER['SERVER_ADDR'];
+            }
+            if ($host == "") {
+                $host = "localhost";
+            }
+            //$url = "http://{$host}{$conf->path_base}framework/{$script}?arg=" . urlencode($args);
+            // TODO: frank fragen ob korrekt
+            $url = "http://{$host}{$_SERVER['REQUEST_URI']}";
+            
+            if (is_callable('curl_init')) {
+                // call script through curl-interface
+                $fp = curl_init($url);
+
+                curl_setopt($fp, CURLOPT_HEADER, false);
+                // hack for "non-blocking" -> has always a timout of 1 second
+                curl_setopt($fp, CURLOPT_TIMEOUT, 1);
+
+                curl_exec($fp);
+                curl_close($fp);
+            // TODO: does not work:
+            } else if (is_callable('fsockopen')) {
+                // call script though fsockopen-interface
+                $urlinfo = parse_url($url);
+
+                if (!isset($urlinfo['port'])) {
+                    $urlinfo['port'] = $_SERVER['SERVER_PORT'];
+                }
+                if (!isset($urlinfo['port'])) {
+                    $urlinfo['port'] = 80;
+                }
+
+                $header .= "GET {$urlinfo['path']}?{$urlinfo['query']} HTTP/1.0\r\n";
+                $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
+                $header .= "Content-Length: 0\r\n\r\n";
+
+                $fp = fsockopen ($urlinfo['host'], $urlinfo['port'], $errno, $errstr, 30);
+                if ($fp) {
+                    fputs ($fp, $header);
+                    fclose($fp);
+                } else {
+                    $log->add_entry("could not execute '$script' by '$url'\n$errorno - $errstr");
+                }
+            // TODO: probably cannot work at all:
+            } else {
+                // call script through fopen -> this is ugly because it's blocking until 
+                // called script is finished or parent script has timed out
+                $fp = fopen($url, 'r');
+                if ($fp) {
+                    fclose($fp);
+                } else {
+                    $log->add_entry("could not execute '$script' by '$url'\n$errorno - $errstr");
+                }
+            }
+        }
+    }
+    // }}}
+    // {{{ getPhpExecutable()
+    private function getPhpExecutable() {
+        // only some shells set this variable
+        $exe = $_SERVER["_"];
+        if (empty($exe) || strpos($exe, "php") === false) {
+            $exe = $this->getPhpExecutableFromPath();
+        }
+        echo($exe);
+        
+        return $exe;
+    }
+    // }}}
+    // {{{ getPhpExecutableFromPath()
+    // see http://stackoverflow.com/questions/3889486/how-to-get-the-path-of-the-php-bin-from-php/3889630#3889630
+    private function getPhpExecutableFromPath() {
+        $paths = explode(PATH_SEPARATOR, getenv('PATH'));
+        foreach ($paths as $path) {
+            $php_executable = $path . DIRECTORY_SEPARATOR . "php" . (isset($_SERVER["WINDIR"]) ? ".exe" : "");
+            if (file_exists($php_executable) && is_file($php_executable)) {
+                return $php_executable;
+            }
+        }
+        return FALSE; // not found
+    }
+    // }}}
+}
+
+
+// run task if called from cli
+$sapi_type = php_sapi_name();
+if (substr($sapi_type, 0, 3) == 'cli') {
+    $dp = new \depage();
+
+    $task_runner = new task_runner($dp->conf, true);
+    $task_runner->run(end($argv));
+}
+
+/* TODO:
+ *  - how to handle errors?
+ *  - restart failed task!? wird erstmal nicht gebraucht!
+ *  - was ist wenn ein einzelner Subtaks Zeitlimit überschreitet?
+ */
+/* vim:set ft=php sw=4 sts=4 fdm=marker : */
+
