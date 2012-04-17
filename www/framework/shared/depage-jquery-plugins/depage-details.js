369fb54a8e41b1c741319bb3279ae4ece2e9163d
Frank Hellenkamp <jonas@depage.net>
2012-04-17 18:56:34 +0200

    depage-jquery-plugins: added new details plugin based on plugin-template

--- www/framework/shared/depage-jquery-plugins/depage-details.js
+++ www/framework/shared/depage-jquery-plugins/depage-details.js
@@ -6,50 +6,67 @@
  * adds details handler to definition-lists
  *
  *
- * copyright (c) 2011 Frank Hellenkamp [jonas@depagecms.net]
+ * copyright (c) 2011-2012 Frank Hellenkamp [jonas@depagecms.net]
  */
-;(function( $ ){
-    $.extend($.depage.fnMethods, {
-        /* {{{ details */
-        /**
-         * @function details()
-         *
-         * adds detail handler to definition lists
-         *
-         * @param selector  elements to which the handler will be attached
-         */
-        details: function(selector) {
-            return this.each( function() {
-                $details = this;
+;(function($){
+    if(!$.depage){
+        $.depage = {};
+    };
+    
+    $.depage.details = function(el, options){
+        // To avoid scope issues, use 'base' instead of 'this' to reference this class from internal events and functions.
+        var base = this;
+        
+        // Access to jQuery and DOM versions of element
+        base.$el = $(el);
+        base.el = el;
+        
+        // Add a reverse reference to the DOM object
+        base.$el.data("depage.details", base);
+        
+        base.init = function(){
+            base.options = $.extend({},$.depage.details.defaultOptions, options);
+            
+            // Put your initialization code here
+            $("dt", base.el).each(function() {
+                var $head = $(this).css({
+                    cursor: "pointer"
+                });
+                var $detail = $head.nextAll("dd:first");
 
-                $("dt", this).each(function() {
-                    var $head = $(this).css({
-                        cursor: "pointer"
-                    });
-                    var $detail = $head.nextAll("dd:first");
+                $(this).prepend("<span class=\"opener\"></span>");
 
-                    $(this).prepend("<span class=\"opener\"></span>");
-
-                    $detail.hide();
-                    $head.click(function() {
-                        if (!$head.hasClass("active")) {
-                            $("dt", $details).removeClass("active");
-                            $head.addClass("active")
-                            $head.siblings("dd").slideUp();
-                            $detail.addClass("active")
-                            $detail.slideDown();
-                        } else {
-                            $detail.removeClass("active")
-                            $detail.slideUp("normal", function() {
-                                $head.removeClass("active");
-                            });
-                        }
-                    });
+                $detail.hide();
+                $head.click(function() {
+                    if (!$head.hasClass("active")) {
+                        $("dt", base.el).removeClass("active");
+                        $head.addClass("active")
+                        $head.siblings("dd").slideUp();
+                        $detail.addClass("active")
+                        $detail.slideDown();
+                    } else {
+                        $detail.removeClass("active")
+                        $detail.slideUp("normal", function() {
+                            $head.removeClass("active");
+                        });
+                    }
                 });
             });
-        }
-        /* }}} */
-    });
-})( jQuery );
-
+        };
+        
+        // Run initializer
+        base.init();
+    };
+    
+    $.depage.details.defaultOptions = {
+        option1: "default"
+    };
+    
+    $.fn.depageDetails = function(options){
+        return this.each(function(){
+            (new $.depage.details(this, options));
+        });
+    };
+    
+})(jQuery);
 /* vim:set ft=javascript sw=4 sts=4 fdm=marker : */
