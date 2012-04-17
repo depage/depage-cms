e3d5a6af878b1f68cf55ca9b0a328b6bba5ec415
Frank Hellenkamp <jonas@depage.net>
2012-04-17 13:40:39 +0200

    updated i18n to merge en_US too

--- www/framework/i18n.sh
+++ www/framework/i18n.sh
@@ -8,19 +8,21 @@ fi
 languages=`ls locale`
 
 echo "extract strings..."
-tempfile=`mktemp /tmp/localize.XXXXXXX`
+tempfile=$(mktemp /tmp/localize.XXXXXXX)
+basepo=$(mktemp /tmp/messages.po.XXXXXXX)
 find . -name "*.tpl" -or -name "*.php" > $tempfile
-xgettext --from-code=UTF-8 -f $tempfile -L PHP -o locale/en_US/LC_MESSAGES/messages.po
+xgettext --from-code=UTF-8 -f $tempfile -L PHP -o - | sed -e 's/charset=CHARSET/charset=UTF-8/' > $basepo
 
-echo "update languages..."
+echo "processing languages"
 for lang in $languages; do
-    echo "update $lang..."
+    echo "updating $lang..."
     if [[ ! -a locale/$lang/LC_MESSAGES/messages.po ]] ; then
         echo "copy template..."
-        cp locale/en_US/LC_MESSAGES/messages.po locale/$lang/LC_MESSAGES/messages.po
+        mkdir -p locale/$lang/LC_MESSAGES
+        cp $basepo locale/$lang/LC_MESSAGES/messages.po
     fi
     cp locale/$lang/LC_MESSAGES/messages.po locale/$lang/LC_MESSAGES/messages_old.po
-    msgmerge locale/$lang/LC_MESSAGES/messages_old.po locale/en_US/LC_MESSAGES/messages.po -o locale/$lang/LC_MESSAGES/messages.po
+    msgmerge locale/$lang/LC_MESSAGES/messages_old.po $basepo -o locale/$lang/LC_MESSAGES/messages.po
     rm locale/$lang/LC_MESSAGES/messages_old.po
 
     msgfmt -o locale/$lang/LC_MESSAGES/messages.mo locale/$lang/LC_MESSAGES/messages.po
