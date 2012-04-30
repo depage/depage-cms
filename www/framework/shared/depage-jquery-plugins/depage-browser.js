e5d4a484fba210754bdb36454bcb99ac297041c9
Frank Hellenkamp <jonas@depage.net>
2012-04-30 19:15:00 +0200

    depage-player: added option to use native controls for iPhone and iPad

---
+++ www/framework/shared/depage-jquery-plugins/depage-browser.js
@@ -0,0 +1,12 @@
+;
+// {{{ jquery.browser.iphone
+jQuery.browser.iphone = function() {
+    return /iphone/.test(navigator.userAgent.toLowerCase());
+}();
+// }}}
+// {{{ jquery.browser.ipad
+jQuery.browser.ipad = function() {
+    return /ipad/.test(navigator.userAgent.toLowerCase());
+}();
+// }}}
+
