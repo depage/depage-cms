34f8e819444297c1bb4ff8445bfd9101c1f6cdf6
Frank Hellenkamp <jonas@depage.net>
2012-05-07 20:16:44 +0200

    depage-jquery-plugins: small bug fixes

--- depage-textbutton.js
+++ depage-textbutton.js
@@ -60,7 +60,7 @@
                             $form.find("input.formSubmit:hidden").remove();
                         }
 
-                        //$form.submit();
+                        $form.submit();
                         $button.click();
                     }
 
