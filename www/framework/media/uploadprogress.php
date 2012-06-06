4b2d84e7fc108dfedb44f7b280956f344a5f5018
Frank Hellenkamp <jonas@depage.net>
2012-06-06 17:03:19 +0200

    changed json-properties of upload progress to match that of nginx upload-progress

--- www/framework/media/uploadprogress.php
+++ www/framework/media/uploadprogress.php
@@ -5,18 +5,14 @@ header('Cache-Control: no-cache, must-revalidate');
 
 $json['apc_enabled'] = function_exists('apc_fetch'); 
 
-if(isset($_GET['APC_UPLOAD_PROGRESS']) && !empty($json['apc_enabled']))
-{
+if(isset($_GET['APC_UPLOAD_PROGRESS']) && !empty($json['apc_enabled'])) {
     $status = apc_fetch('upload_' . $_GET['APC_UPLOAD_PROGRESS']);
     
-    if ($status)
-    {
-        $json['percent'] = (!empty($status['total']))
-            ? $status['current'] * 100 / $status['total']
-            : 0;
+    if ($status) {
+        $json['state'] = "uploading";
+        $json['received'] = $status['current'];
+        $json['size'] = $status['total'];
     }
 }
 
 echo json_encode($json);
-
-?>
