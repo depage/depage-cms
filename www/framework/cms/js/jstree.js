a7ee582773c99187f6ffdc1d9d760d58c14fa128
Frank Hellenkamp <jonas@depage.net>
2012-12-19 17:06:30 +0100

    reformatting

--- www/framework/cms/js/jstree.js
+++ www/framework/cms/js/jstree.js
@@ -1,11 +1,11 @@
 "use strict";
 
 $(function () {
-	$(".jstree-container").each(function () {
-		$(this).jstree({ 
-			// the list of plugins to include
-			"plugins" : ($(this).attr("data-plugins") || "themes pedantic_html_data ui crrm dnd_placeholder types_from_url hotkeys contextmenu span dblclick_rename tooltips select_created_nodes delta_updates add_marker" ).split(" "),
-			// Plugin configuration
+    $(".jstree-container").each(function () {
+        $(this).jstree({ 
+            // the list of plugins to include
+            "plugins" : ($(this).attr("data-plugins") || "themes pedantic_html_data ui crrm dnd_placeholder types_from_url hotkeys contextmenu span dblclick_rename tooltips select_created_nodes delta_updates add_marker" ).split(" "),
+            // Plugin configuration
             delta_updates : {
                 "webSocketURL" : $(this).attr("data-delta-updates-websocket-url"),
                 "fallbackPollURL" : $(this).attr("data-delta-updates-fallback-poll-url"),
