ca109bc1f46597c206b811cfc56013c5e2a644af
Frank Hellenkamp <jonas@depage.net>
2012-12-26 22:11:32 +0100

    db_pdo: added statement to use native prepares

--- www/framework/db/db_pdo.php
+++ www/framework/db/db_pdo.php
@@ -51,6 +51,7 @@ class db_pdo {
 
         // set error mode to exception by default
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
     /* }}} */
 
