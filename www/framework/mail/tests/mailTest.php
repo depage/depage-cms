fbfb4059df918d9dcd2ded844925202318c8777e
Frank Hellenkamp <jonas@depage.net>
2013-10-03 16:19:54 +0200

    enhanced mailTest for multipart messages

--- www/framework/mail/tests/mailTest.php
+++ www/framework/mail/tests/mailTest.php
@@ -27,6 +27,45 @@ class mailTest extends PHPUnit_Framework_TestCase {
         $this->mail     = new mail("sender@domain.com");
     }
     // }}}
+    
+    // {{{ parseMailParts()
+    public function parseMailParts($eml) {
+        $mailparse = mailparse_msg_create();
+        mailparse_msg_parse($mailparse, $eml);
+
+        $structure = mailparse_msg_get_structure($mailparse);
+        $parts = array();
+
+        foreach ($structure as $partId) {
+            $part = mailparse_msg_get_part($mailparse, $partId);
+            $parts[$partId] = mailparse_msg_get_part_data($part);
+        }
+
+        return $parts;
+    }
+    // }}}
+    // {{{ getPartBody()
+    public function getPartBody($eml, $part) {
+        $parts = $this->parseMailParts($eml);
+
+        $start = $parts[$part]['starting-pos-body'];
+        $end = $parts[$part]['ending-pos-body'];
+        $body = substr($eml, $start, $end - $start);
+
+        return quoted_printable_decode($body);
+    }
+    // }}}
+    // {{{ getPartAttachment()
+    public function getPartAttachment($eml, $part) {
+        $parts = $this->parseMailParts($eml);
+
+        $start = $parts[$part]['starting-pos-body'];
+        $end = $parts[$part]['ending-pos-body'];
+        $body = substr($eml, $start, $end - $start);
+
+        return base64_decode($body);
+    }
+    // }}}
 
     // {{{ testSubject()
     public function testSubject() {
@@ -56,28 +95,102 @@ class mailTest extends PHPUnit_Framework_TestCase {
     public function testCC() {
         $this->mail->setCC("cc@domain.com");
 
+        $parts = $this->parseMailParts($this->mail->getEml());
+
         $this->assertRegExp("/^CC: cc@domain.com$/m", $this->mail->getHeaders());
+        $this->assertEquals("cc@domain.com", $parts[1]['headers']['cc']);
     }
     // }}}
     // {{{ testBCC()
     public function testBCC() {
         $this->mail->setBCC("bcc@domain.com");
 
+        $parts = $this->parseMailParts($this->mail->getEml());
+
         $this->assertRegExp("/^BCC: bcc@domain.com$/m", $this->mail->getHeaders());
+        $this->assertEquals("bcc@domain.com", $parts[1]['headers']['bcc']);
     }
     // }}}
     // {{{ testReplyTo()
     public function testReplyTo() {
         $this->mail->setReplyTo("reply@domain.com");
 
+        $parts = $this->parseMailParts($this->mail->getEml());
+
         $this->assertRegExp("/^Reply-To: reply@domain.com$/m", $this->mail->getHeaders());
+        $this->assertEquals("reply@domain.com", $parts[1]['headers']['reply-to']);
     }
     // }}}
     // {{{ testPlainText()
     public function testPlainText() {
-        $this->mail->setText("This is the text with a text line longer than the maximum text width of 75 characters\näöüß");
+        $this->mail->setText("This is the text with a text line longer than the maximum text width of 75 characters\nSpecial Chars: äöüß");
 
-        $this->assertEquals("This is the text with a text line longer than the maximum text width of 75=\n=0Acharacters=0A=C3=A4=C3=B6=C3=BC=C3=9F", $this->mail->getBody());
+        $body = $this->getPartBody($this->mail->getEml(), '1');
+
+        $this->assertEquals("This is the text with a text line longer than the maximum text width of 75\ncharacters\nSpecial Chars: äöüß\n", $body);
+    }
+    // }}}
+    // {{{ testHtmlText
+    public function testHtmlText() {
+        $this->mail->setHtmlText("<p>This is the text with a text line longer than the maximum text width of 75 characters</p>\n<p>Special Chars: äöüß</p>");
+
+        $eml = $this->mail->getEml();
+        $parts = $this->parseMailParts($eml);
+
+        $plainText = $this->getPartBody($eml, '1.1');
+        $htmlText = $this->getPartBody($eml, '1.2');
+
+        $this->assertEquals("text/plain; charset=\"UTF-8\"", $parts['1.1']['headers']['content-type']);
+        $this->assertEquals("This is the text with a text line longer than the maximum text width of 75\ncharacters\nSpecial Chars: äöüß\n", $plainText);
+
+        $this->assertEquals("text/html; charset=\"UTF-8\"", $parts['1.2']['headers']['content-type']);
+        $this->assertEquals("<p>This is the text with a text line longer than the maximum text width of\n75 characters</p>\n<p>Special Chars: äöüß</p>", $htmlText);
+    }
+    // }}}
+    // {{{ testAttachString
+    public function testAttachString() {
+        $this->mail->attachStr("Special Chars: äöüß", "application/octet_stream");
+
+        $eml = $this->mail->getEml();
+        $parts = $this->parseMailParts($eml);
+
+        $attachment = $this->getPartAttachment($eml, '1.2');
+
+        $this->assertEquals("application/octet_stream", $parts['1.2']['headers']['content-type']);
+        $this->assertEquals("Special Chars: äöüß", $attachment);
+    }
+    // }}}
+    // {{{ testAttachFile
+    public function testAttachFile() {
+        $filename = __FILE__;
+
+        $this->mail->attachFile($filename);
+
+        $eml = $this->mail->getEml();
+        $parts = $this->parseMailParts($eml);
+
+        $attachment = $this->getPartAttachment($eml, '1.2');
+
+        $this->assertEquals("application/octet_stream", $parts['1.2']['headers']['content-type']);
+        $this->assertEquals("attachement; filename=\"" . basename($filename) . "\"", $parts['1.2']['headers']['content-disposition']);
+        $this->assertEquals(file_get_contents($filename), $attachment);
+    }
+    // }}}
+    // {{{ testSend
+    public function testSend() {
+        /*
+        rename_function('mail', 'mail_orig');
+        rename_function('mail_mock', 'mail');
+        
+        $this->mail->setSubject("Subject");
+
+        $results = $this->mail->send();
+
+        $this->assertSame('foo@example.com', $results[0]);
+        $this->assertSame('Default Title', $results[1]);
+        $this->assertSame('Default Message', $results[2]);
+        $this->assertSame('Default Message', $results[3]);
+         */
     }
     // }}}
 }
