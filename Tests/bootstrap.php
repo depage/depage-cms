9ad14b8c46e1e834e224ba1bb0013b3bc726ce56
Frank Hellenkamp <jonas@depage.net>
2014-02-25 14:51:15 +0100

    fixed bug in test with missing output directory

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -5,3 +5,5 @@ require_once __DIR__ . '/../Graphics.php';
 require_once __DIR__ . '/graphicsTestClass.php';
 require_once __DIR__ . '/graphics_imagemagickTestClass.php';
 require_once __DIR__ . '/graphics_graphicsmagickTestClass.php';
+
+mkdir(__DIR__ . '/output');
