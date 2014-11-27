2503ae4bbc3a9923157f3adbc3943a69b84ba9a9
Frank Hellenkamp <jonas@depage.net>
2014-11-27 19:07:27 +0100

    Merge commit '0f40147cce208e4a56197f9c1bcfe8bc4d97b440' into depagelegacy

--- Exceptions/MultipleTableNamesException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace depage\DB\Exceptions;
-
-class MultipleTableNamesException extends \Exception
-{
-}
-
-/* vim:set ft=php sw=4 sts=4 fdm=marker et : */
---
+++ Tests/Fixtures/TestFilePart.sql
@@ -0,0 +1,7 @@
+# @tablename test
+
+# @version version 0.1
+    CREATE TABLE test (
+        uid int(10) unsigned NOT NULL DEFAULT '0',
+        pid int(10) unsigned NOT NULL DEFAULT '0'
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
