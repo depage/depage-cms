e1f0c3afb517cf9eb2b62302b05c87e37981ebef
Sebastian Reinhold <sebastian@bitbernd.de>
2015-02-24 19:34:05 +0100

    cleanup
    removed unused getMode function from test-bootstrap

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -48,10 +48,4 @@ class FsSshTestClass extends Depage\Fs\FsSsh
 }
 // }}}
 
-// {{{ getMode
-function getMode($path) {
-    return substr(sprintf('%o', fileperms($path)), -4);
-}
-// }}}
-
 /* vim:set ft=php sw=4 sts=4 fdm=marker et : */
