5ce355b1f791bce316bbc0bf33f2b938e3a5979e
Sebastian Reinhold <sebastian@bitbernd.de>
2015-02-27 19:14:52 +0100

    added ssh key tests

--- Tests/FsSshKeyTest.php
+++ Tests/FsSshKeyTest.php
@@ -22,6 +22,24 @@ class FsSshKeyTest extends TestRemote
         return new FsSshTestClass($newParams);
     }
     // }}}
+    // {{{ createTestClassWithoutKeys
+    public function createTestClassWithoutKeys($override = array())
+    {
+        $params = array(
+            'path' => $GLOBALS['REMOTE_DIR'] . 'Temp',
+            'scheme' => 'ssh2.sftp',
+            'host' => $GLOBALS['REMOTE_HOST'],
+            'port' => '22',
+            'user' => $GLOBALS['REMOTE_USER'],
+            'pass' => $GLOBALS['SSH_KEYPASS'],
+            'fingerprint' => $GLOBALS['SSH_FINGERPRINT'],
+        );
+
+        $newParams = array_merge($params, $override);
+
+        return new FsSshTestClass($newParams);
+    }
+    // }}}
 
     // {{{ testInaccessiblePrivateKeyFile
     /**
@@ -53,23 +71,101 @@ class FsSshKeyTest extends TestRemote
         $fs->ls('*');
     }
     // }}}
-    // {{{ testGeneratePublicKey
-    public function testGeneratePublicKey()
+    // {{{ testPrivateKeyString
+    public function testPrivateKeyString()
     {
         $params = array(
-            'path' => $GLOBALS['REMOTE_DIR'] . 'Temp',
-            'scheme' => 'ssh2.sftp',
-            'host' => $GLOBALS['REMOTE_HOST'],
-            'port' => '22',
-            'user' => $GLOBALS['REMOTE_USER'],
-            'pass' => $GLOBALS['SSH_KEYPASS'],
             'tmp' => '/tmp',
-            'privateKeyFile' => __DIR__ . '/../' . $GLOBALS['SSH_PRIVATE_KEY'],
-            'fingerprint' => $GLOBALS['SSH_FINGERPRINT'],
+            'privateKey' => file_get_contents(__DIR__ . '/../' . $GLOBALS['SSH_PRIVATE_KEY']),
+            'publicKeyFile' => __DIR__ . '/../' . $GLOBALS['SSH_PUBLIC_KEY'],
         );
 
-        $fs =  new FsSshTestClass($params);
+        $fs = $this->createTestClassWithoutKeys($params);
+        $this->assertTrue($fs->test());
+    }
+    // }}}
+    // {{{ testPublicKeyString
+    public function testPublicKeyString()
+    {
+        $params = array(
+            'tmp' => '/tmp',
+            'privateKeyFile' => __DIR__ . '/../' . $GLOBALS['SSH_PRIVATE_KEY'],
+            'publicKey' => file_get_contents(__DIR__ . '/../' . $GLOBALS['SSH_PUBLIC_KEY']),
+        );
+
+        $fs = $this->createTestClassWithoutKeys($params);
+        $this->assertTrue($fs->test());
+    }
+    // }}}
+    // {{{ testInvalidPrivateKeyString
+    /**
+     * @expectedException Depage\Fs\Exceptions\FsException
+     * @expectedExceptionMessage Invalid SSH private key file format "iamnotaprivatesshkey" (PEM format required).
+     */
+    public function testInvalidPrivateKeyString()
+    {
+        $params = array(
+            'tmp' => '/tmp',
+            'privateKey' => 'iamnotaprivatesshkey' ,
+            'publicKeyFile' => __DIR__ . '/../' . $GLOBALS['SSH_PUBLIC_KEY'],
+        );
+
+        $fs = $this->createTestClassWithoutKeys($params);
         $fs->ls('*');
+    }
+    // }}}
+    // {{{ testInvalidPublicKeyString
+    /**
+     * @expectedException Depage\Fs\Exceptions\FsException
+     * @expectedExceptionMessage ssh2_auth_pubkey_file(): Authentication failed for testuser using public key: Invalid public key data
+     */
+    public function testInvalidPublicKeyString()
+    {
+        $params = array(
+            'tmp' => '/tmp',
+            'privateKeyFile' => __DIR__ . '/../' . $GLOBALS['SSH_PRIVATE_KEY'],
+            'publicKey' => 'iamnotapublicsshkey',
+        );
+
+        $fs = $this->createTestClassWithoutKeys($params);
+        $fs->ls('*');
+    }
+    // }}}
+    // {{{ testKeyPairStrings
+    public function testKeyPairStrings()
+    {
+        $params = array(
+            'tmp' => '/tmp',
+            'privateKey' => file_get_contents(__DIR__ . '/../' . $GLOBALS['SSH_PRIVATE_KEY']),
+            'publicKey' => file_get_contents(__DIR__ . '/../' . $GLOBALS['SSH_PUBLIC_KEY']),
+        );
+
+        $fs = $this->createTestClassWithoutKeys($params);
+        $this->assertTrue($fs->test());
+    }
+    // }}}
+    // {{{ testGeneratePublicKeyFromPrivateKeyFile
+    public function testGeneratePublicKeyFromPrivateKeyFile()
+    {
+        $params = array(
+            'tmp' => '/tmp',
+            'privateKeyFile' => __DIR__ . '/../' . $GLOBALS['SSH_PRIVATE_KEY'],
+        );
+
+        $fs = $this->createTestClassWithoutKeys($params);
+        $this->assertTrue($fs->test());
+    }
+    // }}}
+    // {{{ testGeneratePublicKeyFromPrivateKeyString
+    public function testGeneratePublicKeyFromPrivateKeyString()
+    {
+        $params = array(
+            'tmp' => '/tmp',
+            'privateKey' => file_get_contents(__DIR__ . '/../' . $GLOBALS['SSH_PRIVATE_KEY']),
+        );
+
+        $fs = $this->createTestClassWithoutKeys($params);
+        $this->assertTrue($fs->test());
     }
     // }}}
 }
