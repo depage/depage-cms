4ff65099352e9088a69d9b1d6a10f1236305e7e0
Sebastian Reinhold <sebastian@bitbernd.de>
2015-02-27 18:57:21 +0100

    refactoring

--- PrivateSshKey.php
+++ PrivateSshKey.php
@@ -22,24 +22,28 @@ class PrivateSshKey extends PublicSshKey
      */
     public function extractPublicKey($tmpDir)
     {
-        function sshEncodeBuffer($buffer)
-        {
-            $len = strlen($buffer);
-            if (ord($buffer[0]) & 0x80) {
-                $len++;
-                $buffer = "\x00" . $buffer;
-            }
-
-            return pack('Na*', $len, $buffer);
-        }
-
         $details = openssl_pkey_get_details($this->key);
         $buffer = pack('N', 7) . 'ssh-rsa' .
-            sshEncodeBuffer($details['rsa']['e']) .
-            sshEncodeBuffer($details['rsa']['n']);
+            $this->sshEncodeBuffer($details['rsa']['e']) .
+            $this->sshEncodeBuffer($details['rsa']['n']);
         $publicKeyString = 'ssh-rsa ' . base64_encode($buffer) . "\n";
 
         return new PublicSshKey($publicKeyString, $tmpDir);
+    }
+    // }}}
+    // {{{ sshEncodeBuffer
+    /**
+     * from http://stackoverflow.com/questions/5524121/converting-an-openssl-generated-rsa-public-key-to-openssh-format-php
+     */
+    protected function sshEncodeBuffer($buffer)
+    {
+        $len = strlen($buffer);
+        if (ord($buffer[0]) & 0x80) {
+            $len++;
+            $buffer = "\x00" . $buffer;
+        }
+
+        return pack('Na*', $len, $buffer);
     }
     // }}}
 }
