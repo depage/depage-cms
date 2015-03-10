257703dcabc187cab8d4fb51831a50d276fd7461
Sebastian Reinhold <sebastian@bitbernd.de>
2015-03-10 18:24:29 +0100

    test extractFileName

--- Tests/FsTest.php
+++ Tests/FsTest.php
@@ -31,6 +31,24 @@ class FsTest extends PHPUnit_Framework_TestCase
         return $result;
     }
     // }}}
+    // {{{ invokeCleanUrl
+    public function invokeCleanUrl($url)
+    {
+        return $this->invoke($this->fs, 'cleanUrl', array($url));
+    }
+    // }}}
+    // {{{ invokeParseUrl
+    public function invokeParseUrl($url)
+    {
+        return $this->invoke($this->fs, 'parseUrl', array($url));
+    }
+    // }}}
+    // {{{ invokeExtractFileName
+    public function invokeExtractFileName($path)
+    {
+        return $this->invoke($this->fs, 'extractFileName', array($path));
+    }
+    // }}}
 
     // {{{ testCleanUrl
     public function testCleanUrl()
@@ -62,9 +80,9 @@ class FsTest extends PHPUnit_Framework_TestCase
             'path'=>'/path/to/file',
             'scheme'=>'file',
         );
-        $this->assertEquals($expected, $this->invoke($this->fs, 'parseUrl', array('file:///path/to/file')));
+        $this->assertEquals($expected, $this->invokeParseUrl('file:///path/to/file'));
 
-        $this->assertEquals(array('path'=>'/path/to/file'), $this->invoke($this->fs, 'parseUrl', array('/path/to/file')));
+        $this->assertEquals(array('path'=>'/path/to/file'), $this->invokeParseUrl('/path/to/file'));
 
         $expected = array(
             'path'      => '/path/to/file',
@@ -74,38 +92,67 @@ class FsTest extends PHPUnit_Framework_TestCase
             'host'      => 'testHost',
             'port'      => '42',
         );
-        $this->assertEquals($expected, $this->invoke($this->fs, 'parseUrl', array('ftp://testUser:testPass@testHost:42/path/to/file')));
+        $this->assertEquals($expected, $this->invokeParseUrl('ftp://testUser:testPass@testHost:42/path/to/file'));
     }
     // }}}
     // {{{ testParseUrlPath
     public function testParseUrlPath()
     {
-        $this->assertEquals(array('path'=>''),          $this->invoke($this->fs, 'parseUrl', array('')));
-        $this->assertEquals(array('path'=>'abc'),       $this->invoke($this->fs, 'parseUrl', array('abc')));
-        $this->assertEquals(array('path'=>'a[bd]c'),    $this->invoke($this->fs, 'parseUrl', array('a[bd]c')));
-        $this->assertEquals(array('path'=>'abc*'),      $this->invoke($this->fs, 'parseUrl', array('abc*')));
-        $this->assertEquals(array('path'=>'*abc'),      $this->invoke($this->fs, 'parseUrl', array('*abc')));
-        $this->assertEquals(array('path'=>'*abc*'),     $this->invoke($this->fs, 'parseUrl', array('*abc*')));
-        $this->assertEquals(array('path'=>'*'),         $this->invoke($this->fs, 'parseUrl', array('*')));
-        $this->assertEquals(array('path'=>'**'),        $this->invoke($this->fs, 'parseUrl', array('**')));
-        $this->assertEquals(array('path'=>'abc?'),      $this->invoke($this->fs, 'parseUrl', array('abc?')));
-        $this->assertEquals(array('path'=>'ab?c'),      $this->invoke($this->fs, 'parseUrl', array('ab?c')));
-        $this->assertEquals(array('path'=>'?abc'),      $this->invoke($this->fs, 'parseUrl', array('?abc')));
-        $this->assertEquals(array('path'=>'?abc?'),     $this->invoke($this->fs, 'parseUrl', array('?abc?')));
-        $this->assertEquals(array('path'=>'?'),         $this->invoke($this->fs, 'parseUrl', array('?')));
-        $this->assertEquals(array('path'=>'??'),        $this->invoke($this->fs, 'parseUrl', array('??')));
-        $this->assertEquals(array('path'=>'a&b'),       $this->invoke($this->fs, 'parseUrl', array('a&b')));
-        $this->assertEquals(array('path'=>'&'),         $this->invoke($this->fs, 'parseUrl', array('&')));
-        $this->assertEquals(array('path'=>'&&'),        $this->invoke($this->fs, 'parseUrl', array('&&')));
+        $this->assertEquals(array('path'=>''),          $this->invokeParseUrl(''));
+        $this->assertEquals(array('path'=>'abc'),       $this->invokeParseUrl('abc'));
+        $this->assertEquals(array('path'=>'a[bd]c'),    $this->invokeParseUrl('a[bd]c'));
+        $this->assertEquals(array('path'=>'abc*'),      $this->invokeParseUrl('abc*'));
+        $this->assertEquals(array('path'=>'*abc'),      $this->invokeParseUrl('*abc'));
+        $this->assertEquals(array('path'=>'*abc*'),     $this->invokeParseUrl('*abc*'));
+        $this->assertEquals(array('path'=>'*'),         $this->invokeParseUrl('*'));
+        $this->assertEquals(array('path'=>'**'),        $this->invokeParseUrl('**'));
+        $this->assertEquals(array('path'=>'abc?'),      $this->invokeParseUrl('abc?'));
+        $this->assertEquals(array('path'=>'ab?c'),      $this->invokeParseUrl('ab?c'));
+        $this->assertEquals(array('path'=>'?abc'),      $this->invokeParseUrl('?abc'));
+        $this->assertEquals(array('path'=>'?abc?'),     $this->invokeParseUrl('?abc?'));
+        $this->assertEquals(array('path'=>'?'),         $this->invokeParseUrl('?'));
+        $this->assertEquals(array('path'=>'??'),        $this->invokeParseUrl('??'));
+        $this->assertEquals(array('path'=>'a&b'),       $this->invokeParseUrl('a&b'));
+        $this->assertEquals(array('path'=>'&'),         $this->invokeParseUrl('&'));
+        $this->assertEquals(array('path'=>'&&'),        $this->invokeParseUrl('&&'));
     }
     // }}}
     // {{{ testCleanUrlFile
     public function testCleanUrlFile()
     {
         $this->invoke($this->fs, 'lateConnect');
-        $this->assertEquals('file://' . getcwd() . '/path/to/file', $this->invoke($this->fs, 'cleanUrl', array('file://' . getcwd() . '/path/to/file')));
-        $this->assertEquals('file://' . getcwd() . '/path/to/file', $this->invoke($this->fs, 'cleanUrl', array('path/to/file')));
-        $this->assertEquals('file://' . getcwd() . '/path/to/file', $this->invoke($this->fs, 'cleanUrl', array(getcwd() . '/path/to/file')));
+        $this->assertEquals('file://' . getcwd() . '/path/to/file', $this->invokeCleanUrl('file://' . getcwd() . '/path/to/file'));
+        $this->assertEquals('file://' . getcwd() . '/path/to/file', $this->invokeCleanUrl('path/to/file'));
+        $this->assertEquals('file://' . getcwd() . '/path/to/file', $this->invokeCleanUrl(getcwd() . '/path/to/file'));
+    }
+    // }}}
+    // {{{ testExtractFileName
+    public function testExtractFileName()
+    {
+        $this->assertEquals('filename.extension', $this->invokeExtractFileName('scheme://path/to/filename.extension'));
+        $this->assertEquals('filename.extension', $this->invokeExtractFileName('path/to/filename.extension'));
+        $this->assertEquals('filename.extension', $this->invokeExtractFileName('/filename.extension'));
+        $this->assertEquals('filename.extension', $this->invokeExtractFileName('filename.extension'));
+
+        $this->assertEquals('filename', $this->invokeExtractFileName('scheme://path/to/filename'));
+        $this->assertEquals('filename', $this->invokeExtractFileName('path/to/filename'));
+        $this->assertEquals('filename', $this->invokeExtractFileName('/filename'));
+        $this->assertEquals('filename', $this->invokeExtractFileName('filename'));
+
+        $this->assertEquals('filename.stuff.extension', $this->invokeExtractFileName('scheme://path/to/filename.stuff.extension'));
+        $this->assertEquals('filename.stuff.extension', $this->invokeExtractFileName('path/to/filename.stuff.extension'));
+        $this->assertEquals('filename.stuff.extension', $this->invokeExtractFileName('/filename.stuff.extension'));
+        $this->assertEquals('filename.stuff.extension', $this->invokeExtractFileName('filename.stuff.extension'));
+
+        $this->assertEquals('filename.', $this->invokeExtractFileName('scheme://path/to/filename.'));
+        $this->assertEquals('filename.', $this->invokeExtractFileName('path/to/filename.'));
+        $this->assertEquals('filename.', $this->invokeExtractFileName('/filename.'));
+        $this->assertEquals('filename.', $this->invokeExtractFileName('filename.'));
+
+        $this->assertEquals('.extension', $this->invokeExtractFileName('scheme://path/to/.extension'));
+        $this->assertEquals('.extension', $this->invokeExtractFileName('path/to/.extension'));
+        $this->assertEquals('.extension', $this->invokeExtractFileName('/.extension'));
+        $this->assertEquals('.extension', $this->invokeExtractFileName('.extension'));
     }
     // }}}
 }
