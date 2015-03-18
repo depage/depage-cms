813ec63647b023a608f3188c6dba6aab375c0fab
Frank Hellenkamp <jonas@depage.net>
2015-03-18 18:30:52 +0100

    fixed base64 encoding for svgEncode

--- helper/functions.rb
+++ helper/functions.rb
@@ -4,13 +4,13 @@ require 'base64'
 module Sass::Script::Functions
     def base64Encode(string)
         assert_type string, :String
-        Sass::Script::String.new(Base64.encode64(string.value))
+        Sass::Script::String.new(Base64.strict_encode64(string.value))
     end
     declare :base64Encode, :args => [:string]
 
     def svgEncode(string)
         assert_type string, :String
-        data_url = "url('data:image/svg+xml;charset=utf-8," + Base64.encode64(string.value) + "')"
+        data_url = "url(\"data:image/svg+xml;charset=utf-8;base64," + Base64.strict_encode64(string.value) + "\")"
         Sass::Script::String.new(data_url)
     end
     declare :svgEncode, :args => [:string]
