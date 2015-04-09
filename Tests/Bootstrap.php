d8c903eff49befb62c8d310ce5d2fc4d0d34a580
Sebastian Reinhold <sebastian@bitbernd.de>
2015-04-09 20:43:46 +0200

    prepared bootstrap for autoloader

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -1,9 +1,6 @@
 <?php
 
-require_once(__DIR__ . '/../depage-cache/Cache.php');
-require_once(__DIR__ . '/../depage-db/Pdo.php');
-require_once(__DIR__ . '/../depage-db/Schema.php');
-require_once(__DIR__ . '/../depage-xml/Document.php');
+require_once(__DIR__ . '/vendor/autoload.php');
 
 require_once(__DIR__ . '/../XmlGetter.php');
 require_once(__DIR__ . '/../Document.php');
