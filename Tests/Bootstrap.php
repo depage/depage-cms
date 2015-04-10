28eae6aad58166c130340d2945efa76701e188e7
Sebastian Reinhold <sebastian@bitbernd.de>
2015-04-10 14:49:04 +0200

    changed test dependency setup

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once(__DIR__ . '/vendor/autoload.php');
+// @todo reenable once composer installs are available
+//require_once(__DIR__ . '/../vendor/autoload.php');
 
 require_once(__DIR__ . '/../XmlGetter.php');
 require_once(__DIR__ . '/../Document.php');
