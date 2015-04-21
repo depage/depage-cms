9b344ce26acd85801878a0fd14ed4f9c6aa3142a
Sebastian Reinhold <sebastian@bitbernd.de>
2015-04-21 16:22:30 +0200

    test getDoc

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -9,11 +9,13 @@ require_once(__DIR__ . '/../vendor/depage-cache/Providers/Uncached.php');
 require_once(__DIR__ . '/../vendor/depage-db/Pdo.php');
 require_once(__DIR__ . '/../vendor/depage-db/Schema.php');
 require_once(__DIR__ . '/../vendor/depage-db/SqlParser.php');
+require_once(__DIR__ . '/../vendor/depage-xml/Document.php');
 
 require_once(__DIR__ . '/../XmlGetter.php');
 require_once(__DIR__ . '/../Document.php');
 require_once(__DIR__ . '/../XmlDb.php');
 require_once(__DIR__ . '/../XmlNs.php');
+require_once(__DIR__ . '/../XmlDocTypes/Base.php');
 
 const DEPAGE_CACHE_PATH = 'cache';
 const DEPAGE_BASE = 'base';
--- Tests/XmlDbTest.php
+++ Tests/XmlDbTest.php
@@ -59,6 +59,17 @@ class XmlDbTest extends Generic_Tests_DatabaseTestCase
         $this->assertEquals(1, $this->xmldb->docExists(1));
     }
     // }}}
+    // {{{ testGetDoc()
+    public function testGetDoc() {
+        $xmlStr = '<dpg:pages xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" db:id="1" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid="" db:name=""><pg:page file_type="html" multilang="true" name="Home" db:dataid="3" db:id="2"><pg:page file_type="html" multilang="true" name="Subpage" db:dataid="4" db:id="6"/><pg:page file_type="html" multilang="true" name="Subpage 2" db:dataid="5" db:id="7"/><pg:folder file_type="html" multilang="true" name="Subpage" db:dataid="7" db:id="9"/>bla bla blub <pg:page file_type="html" multilang="true" name="bla blub" db:dataid="6" db:id="8">bla bla bla </pg:page></pg:page></dpg:pages>';
+
+        $searches = array(1, '1', 'pages');
+
+        foreach ($searches as $search) {
+            $this->assertXmlStringEqualsXmlString($xmlStr, $this->xmldb->getDoc($search)->getXml());
+        }
+    }
+    // }}}
 }
 
 /* vim:set ft=php fenc=UTF-8 sw=4 sts=4 fdm=marker et : */
