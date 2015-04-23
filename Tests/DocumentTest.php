c642fec1d37045378c437d419744ad4d41d8fb8d
Sebastian Reinhold <sebastian@bitbernd.de>
2015-04-23 18:54:15 +0200

    test getNodeIdsbyXpath makes smaller test cases

--- Tests/DocumentTest.php
+++ Tests/DocumentTest.php
@@ -2,130 +2,117 @@
 
 class DocumentTest extends Generic_Tests_DatabaseTestCase
 {
-    protected $xmldb;
-
+    // {{{ variables
+    protected $xmlDb;
+    protected $doc;
+    // }}}
     // {{{ setUp
     protected function setUp()
     {
         parent::setUp();
 
         // get cache instance
-        $cache = Depage\Cache\Cache::factory("xmldb", array("disposition" => "uncached"));
+        $cache = Depage\Cache\Cache::factory('xmlDb', array('disposition' => 'uncached'));
 
         // get xmldb instance
-        $this->xmldb = new Depage\XmlDb\XmlDb($this->pdo->prefix . "_proj_test", $this->pdo, $cache, array(
-            "root",
-            "child",
+        $this->xmlDb = new Depage\XmlDb\XmlDb($this->pdo->prefix . '_proj_test', $this->pdo, $cache, array(
+            'root',
+            'child',
         ));
+
+        $this->doc = $this->xmlDb->getDoc(1);
     }
     // }}}
 
     // {{{ testGetSubDocByXpathByNameAll
     public function testGetSubDocByXpathByNameAll()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('//pg:page');
+        $subDoc = $this->doc->getSubDocByXpath('//pg:page');
 
         $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="Home" db:dataid="3" db:id="2" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid=""><pg:page file_type="html" multilang="true" name="Subpage" db:dataid="4" db:id="6"/><pg:page file_type="html" multilang="true" name="Subpage 2" db:dataid="5" db:id="7"/><pg:folder file_type="html" multilang="true" name="Subpage" db:dataid="7" db:id="9"/>bla bla blub <pg:page file_type="html" multilang="true" name="bla blub" db:dataid="6" db:id="8">bla bla bla </pg:page></pg:page>';
 
         $this->assertXmlStringEqualsXmlString($expected, $subDoc);
     }
     // }}}
-    // {{{ testGetSubDocByXpathByNameAllWithAttribute
-    public function testGetSubDocByXpathByNameAllWithAttribute()
+
+    // {{{ testGetNodeIdsByXpathByNameAll
+    public function testGetNodeIdsByXpathByNameAll()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('//pg:page[@name = \'bla blub\']');
+        $ids = $this->doc->getNodeIdsByXpath('//pg:page');
 
-        $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="bla blub" db:dataid="6" db:id="8" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid="">bla bla bla </pg:page>';
-
-        $this->assertXmlStringEqualsXmlString($expected, $subDoc);
+        $this->assertEquals(array('2', '6', '7', '8'), $ids);
     }
     // }}}
-    // {{{ testGetSubDocByXpathByNameAllWithChild
-    public function testGetSubDocByXpathByNameAllWithChild()
+    // {{{ testGetNodeIdsByXpathByNameAllWithAttribute
+    public function testGetNodeIdsByXpathByNameAllWithAttribute()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('/dpg:pages/pg:page');
+        $ids = $this->doc->getNodeIdsByXpath('//pg:page[@name = \'bla blub\']');
 
-       $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="Home" db:dataid="3" db:id="2" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid=""><pg:page file_type="html" multilang="true" name="Subpage" db:dataid="4" db:id="6"/><pg:page file_type="html" multilang="true" name="Subpage 2" db:dataid="5" db:id="7"/><pg:folder file_type="html" multilang="true" name="Subpage" db:dataid="7" db:id="9"/>bla bla blub <pg:page file_type="html" multilang="true" name="bla blub" db:dataid="6" db:id="8">bla bla bla </pg:page></pg:page>';
-
-        $this->assertXmlStringEqualsXmlString($expected, $subDoc);
+        $this->assertEquals(array('8'), $ids);
     }
     // }}}
-    // {{{ testGetSubDocByXpathByNameAndPosition
-    public function testGetSubDocByXpathByNameAndPosition()
+    // {{{ testGetNodeIdsByXpathByNameAllWithChild
+    public function testGetNodeIdsByXpathByNameAllWithChild()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('/dpg:pages/pg:page/pg:page[3]');
+        $ids = $this->doc->getNodeIdsByXpath('/dpg:pages/pg:page');
 
-        $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="bla blub" db:dataid="6" db:id="8" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid="">bla bla bla </pg:page>';
-
-        $this->assertXmlStringEqualsXmlString($expected, $subDoc);
+        $this->assertEquals(array('2'), $ids);
     }
     // }}}
-    // {{{ testGetSubDocByXpathByNameAndAttribute
-    public function testGetSubDocByXpathByNameAndAttribute()
+    // {{{ testGetNodeIdsByXpathByNameAndPosition
+    public function testGetNodeIdsByXpathByNameAndPosition()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('/dpg:pages/pg:page/pg:page[@name]');
+        $ids = $this->doc->getNodeIdsByXpath('/dpg:pages/pg:page/pg:page[3]');
 
-        $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="Subpage" db:dataid="4" db:id="6" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid=""/>';
-
-        $this->assertXmlStringEqualsXmlString($expected, $subDoc);
+        $this->assertEquals(array('8'), $ids);
     }
     // }}}
-    // {{{ testGetSubDocByXpathByNameAndAttributeWithValue
-    public function testGetSubDocByXpathByNameAndAttributeWithValue()
+    // {{{ testGetNodeIdsByXpathByNameAndAttribute
+    public function testGetNodeIdsByXpathByNameAndAttribute()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('/dpg:pages/pg:page/pg:page[@name = \'Subpage\']');
+        $ids = $this->doc->getNodeIdsByXpath('/dpg:pages/pg:page/pg:page[@name]');
 
-        $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="Subpage" db:dataid="4" db:id="6" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid=""/>';
-
-        $this->assertXmlStringEqualsXmlString($expected, $subDoc);
+        $this->assertEquals(array('6', '7', '8'), $ids);
     }
     // }}}
-    // {{{ testGetSubDocByXpathByWildcardAndAttributeWithValue
-    public function testGetSubDocByXpathByWildcardAndAttributeWithValue()
+    // {{{ testGetNodeIdsByXpathByNameAndAttributeWithValue
+    public function testGetNodeIdsByXpathByNameAndAttributeWithValue()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('/dpg:pages/pg:page/*[@name = \'Subpage\']');
+        $ids = $this->doc->getNodeIdsByXpath('/dpg:pages/pg:page/pg:page[@name = \'Subpage\']');
 
-        $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="Subpage" db:dataid="4" db:id="6" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid=""/>';
-
-        $this->assertXmlStringEqualsXmlString($expected, $subDoc);
+        $this->assertEquals(array('6'), $ids);
     }
     // }}}
-    // {{{ testGetSubDocByXpathByWildcardNsAndAttributeWithValue
-    public function testGetSubDocByXpathByWildcardNsAndAttributeWithValue()
+    // {{{ testGetNodeIdsByXpathByWildcardAndAttributeWithValue
+    public function testGetNodeIdsByXpathByWildcardAndAttributeWithValue()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('/dpg:pages/pg:page/*:page[@name = \'Subpage\']');
+        $ids = $this->doc->getNodeIdsByXpath('/dpg:pages/pg:page/*[@name = \'Subpage\']');
 
-        $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="Subpage" db:dataid="4" db:id="6" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid=""/>';
-
-        $this->assertXmlStringEqualsXmlString($expected, $subDoc);
+        $this->assertEquals(array('6', '9'), $ids);
     }
     // }}}
-    // {{{ testGetSubDocByXpathByWildcardNameAndAttributeWithValue
-    public function testGetSubDocByXpathByWildcardNameAndAttributeWithValue()
+    // {{{ testGetNodeIdsByXpathByWildcardNsAndAttributeWithValue
+    public function testGetNodeIdsByXpathByWildcardNsAndAttributeWithValue()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('/dpg:pages/pg:page/pg:*[@name = \'Subpage\']');
+        $ids = $this->doc->getNodeIdsByXpath('/dpg:pages/pg:page/*:page[@name = \'Subpage\']');
 
-        $expected = '<pg:page xmlns:db="http://cms.depagecms.net/ns/database" xmlns:dpg="http://www.depagecms.net/ns/depage" xmlns:edit="http://www.depagecms.net/ns/edit" xmlns:pg="http://www.depagecms.net/ns/page" xmlns:sec="http://www.depagecms.net/ns/section" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" file_type="html" multilang="true" name="Subpage" db:dataid="4" db:id="6" db:lastchange="0000-00-00 00:00:00" db:lastchangeUid=""/>';
-
-        $this->assertXmlStringEqualsXmlString($expected, $subDoc);
+        $this->assertEquals(array('6'), $ids);
     }
     // }}}
-    // {{{ testGetSubDocByXpathNoResult
-    public function testGetSubDocByXpathNoResult()
+    // {{{ testGetNodeIdsByXpathByWildcardNameAndAttributeWithValue
+    public function testGetNodeIdsByXpathByWildcardNameAndAttributeWithValue()
     {
-        $doc = $this->xmldb->getDoc(1);
-        $subDoc = $doc->getSubDocByXpath('/nonode');
+        $ids = $this->doc->getNodeIdsByXpath('/dpg:pages/pg:page/pg:*[@name = \'Subpage\']');
 
-        $this->assertFalse($subDoc);
+        $this->assertEquals(array('6', '9'), $ids);
+    }
+    // }}}
+    // {{{ testGetNodeIdsByXpathNoResult
+    public function testGetNodeIdsByXpathNoResult()
+    {
+        $ids = $this->doc->getNodeIdsByXpath('/nonode');
+
+        $this->assertEquals(array(), $ids);
     }
     // }}}
 }
