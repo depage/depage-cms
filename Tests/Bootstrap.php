9c42d0134509ece4469058a0ce83a5d993f9035d
Sebastian Reinhold <sebastian@bitbernd.de>
2015-04-24 14:25:49 +0200

    use composer autoloader in tests

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -1,10 +1,6 @@
 <?php
 
-require_once(__DIR__ . "/../Cache.php");
-require_once(__DIR__ . "/../Providers/File.php");
-require_once(__DIR__ . "/../Providers/Memcache.php");
-require_once(__DIR__ . "/../Providers/Memcached.php");
-require_once(__DIR__ . "/../Providers/Redis.php");
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 const DEPAGE_CACHE_PATH = "cache";
 const DEPAGE_BASE = "base";
