7e000883f55c9e3a3e9f9680dff1efe00dec927b
Frank Hellenkamp <jonas@depage.net>
2015-05-18 12:58:13 +0200

    added hack to depage-live-filter to work with depage details

--- www/framework/shared/depage-jquery-plugins/depage-live-filter.js
+++ www/framework/shared/depage-jquery-plugins/depage-live-filter.js
@@ -2,15 +2,15 @@
     if(!$.depage){
         $.depage = {};
     }
-    
+
     $.depage.liveFilter = function(el, itemSelector, searchSelector, options){
         // To avoid scope issues, use 'base' instead of 'this' to reference this class from internal events and functions.
         var base = this;
-        
+
         // Access to jQuery and DOM versions of element
         base.$el = $(el);
         base.el = el;
-        
+
         if (base.$el.data("depage.liveFilter") !== undefined) {
             // test if this is already a liveFilter object
             // @todo remove and re-add liveFilter when called with different options
@@ -23,17 +23,17 @@
 
         var $items = null;
         var keywords = [];
-        
+
         // {{{ init()
         base.init = function() {
             if( typeof( itemSelector ) === "undefined" || itemSelector === null ) itemSelector = "default"; // @todo throw error, not optional!
             if( typeof( searchSelector ) === "undefined" || searchSelector === null ) searchSelector = "default"; // @todo throw error, not optional!
-            
+
             base.itemSelector = itemSelector;
             base.searchSelector = searchSelector;
-            
+
             base.options = $.extend({},$.depage.liveFilter.defaultOptions, options);
-            
+
             // Put your initialization code here
             var $container = $("<div class=\"" + base.options.inputClass +  "\"></div>").insertBefore(base.$el);
             base.$input = $("<input type=\"search\" placeholder=\"" + base.options.placeholder + "\">").prependTo($container);
@@ -65,30 +65,38 @@
                     found = found && keywords[i].indexOf(values[j]) != -1;
                 }
 
+                var $item = $items.eq(i);
+
+                // @todo add events when showing/hiding elements
                 if (found) {
-                    $items.eq(i).show();
+                    $item.show();
                 } else {
-                    $items.eq(i).hide();
+                    $item.hide();
+
+                    // @todo adjust live filter to work with filtering depage-details -> only a hack
+                    if ($item.is("dt")) {
+                        $item.next("dd").hide();
+                    }
                 }
             }
         };
         // }}}
-        
+
         // Run initializer
         base.init();
     };
-    
+
     $.depage.liveFilter.defaultOptions = {
         inputClass: "depage-live-filter",
         placeholder: "Search"
     };
-    
+
     $.fn.depageLiveFilter = function(itemSelector, searchSelector, options){
         return this.each(function(){
             (new $.depage.liveFilter(this, itemSelector, searchSelector, options));
         });
     };
-    
+
 })(jQuery);
 
 /* vim:set ft=javascript sw=4 sts=4 fdm=marker : */
