9e56311bd6a38746567ffd02db332326baf6967e
Sebastian Reinhold <sebastian@bitbernd.de>
2015-06-02 17:25:49 +0200

    document: test getChildIdsByName

--- Tests/DocumentTestClass.php
+++ Tests/DocumentTestClass.php
@@ -8,6 +8,10 @@ class DocumentTestClass extends \Depage\XmlDb\Document
     {
         return parent::getPosById($id);
     }
+    public function getChildIdsByName($parent_id, $node_ns = '', $node_name = '', $attr_cond = null, $only_element_nodes = false)
+    {
+        return parent::getChildIdsByName($parent_id, $node_ns, $node_name, $attr_cond, $only_element_nodes);
+    }
 }
 
 /* vim:set ft=php fenc=UTF-8 sw=4 sts=4 fdm=marker et : */
