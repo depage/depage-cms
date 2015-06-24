d3203b578a23a5d0fc79a4255f7f8a9132c4b855
Frank Hellenkamp <jonas@depage.net>
2015-06-24 15:04:22 +0200

    updated i18n to be more flexible about the locale directory

--- www/framework/i18n.sh
+++ www/framework/i18n.sh
@@ -3,62 +3,76 @@ if ! test -d locale
 then
     # search for locale dir if not in current path
     localeDir=$( find . -type d -name locale | grep locale )
-    cd $localeDir/..
+else
+    localeDir=locale
 fi
 
-if ! test -d locale
+if ! test -d $localeDir
 then
-    echo "run this script in the parent directory of your locale directory"
+    echo "no locale dir found below current path"
     exit 1
+else
+    echo "found locale directory in $localeDir"
 fi
 
-languages=`ls locale`
+languages=$( ls $localeDir )
 
 echo "extract strings..."
 filesPHP=$(mktemp /tmp/localize.XXXXXXX)
 filesXML=$(mktemp /tmp/localize.XXXXXXX)
+filesJS=$(mktemp /tmp/localize.XXXXXXX)
 potAll=$(mktemp /tmp/localize.XXXXXXX)
+potJS=$(mktemp /tmp/localize.XXXXXXX)
 
+# find php files
 find . -name "*.php" -or -name "*.tpl" > $filesPHP
 xgettext \
     -f $filesPHP \
     --from-code=UTF-8 -L PHP -o - \
     | sed -e 's/charset=CHARSET/charset=UTF-8/' > $potAll
 
+# find xml files for navigation
 find . -name "nav*.xml" > $filesXML
 xgettext \
     -f $filesXML \
     --from-code=UTF-8 \
     -L perl -k -k"name" -j -o $potAll
 
+# find js files
+find . -name "*.js" > $filesJS
+xgettext \
+    -f $filesJS \
+    --from-code=UTF-8 \
+    -L perl -o $potJS
+
+
 echo "processing languages"
 for lang in $languages; do
     echo "updating $lang..."
-    if [[ ! -a locale/$lang/LC_MESSAGES/messages.po ]] ; then
+    if [[ ! -a $localeDir/$lang/LC_MESSAGES/messages.po ]] ; then
         echo "copy template..."
-        mkdir -p locale/$lang/LC_MESSAGES
-        cp $potAll locale/$lang/LC_MESSAGES/messages.po
+        mkdir -p $localeDir/$lang/LC_MESSAGES
+        cp $potAll $localeDir/$lang/LC_MESSAGES/messages.po
     fi
-    cp locale/$lang/LC_MESSAGES/messages.po locale/$lang/LC_MESSAGES/messages_old.po
-    cp locale/$lang/LC_MESSAGES/messages.po locale/$lang/LC_MESSAGES/messages_old_bak.po
-    msgmerge locale/$lang/LC_MESSAGES/messages_old.po $potAll -o locale/$lang/LC_MESSAGES/messages.po
+    cp $localeDir/$lang/LC_MESSAGES/messages.po $localeDir/$lang/LC_MESSAGES/messages_old.po
+    cp $localeDir/$lang/LC_MESSAGES/messages.po $localeDir/$lang/LC_MESSAGES/messages_old_bak.po
+    msgmerge $localeDir/$lang/LC_MESSAGES/messages_old.po $potAll -o $localeDir/$lang/LC_MESSAGES/messages.po
     if [[ -a framework/locale/$lang/LC_MESSAGES/messages.po ]] ; then
-        cp locale/$lang/LC_MESSAGES/messages.po locale/$lang/LC_MESSAGES/messages_old.po
-        msgcat locale/$lang/LC_MESSAGES/messages_old.po framework/locale/$lang/LC_MESSAGES/messages.po -o locale/$lang/LC_MESSAGES/messages.po
+        cp $localeDir/$lang/LC_MESSAGES/messages.po $localeDir/$lang/LC_MESSAGES/messages_old.po
+        msgcat --use-first $localeDir/$lang/LC_MESSAGES/messages_old.po framework/$localeDir/$lang/LC_MESSAGES/messages.po -o $localeDir/$lang/LC_MESSAGES/messages.po
     fi
 
-    chanchedLines=$( diff -I ".POT-Creation-Date:.*" -I "#.*" locale/$lang/LC_MESSAGES/messages_old_bak.po locale/$lang/LC_MESSAGES/messages.po | grep -v '^[<>-]' | wc -l | grep -o "[0-9]\+" )
+    chanchedLines=$( diff -I ".POT-Creation-Date:.*" -I "#.*" $localeDir/$lang/LC_MESSAGES/messages_old_bak.po $localeDir/$lang/LC_MESSAGES/messages.po | grep -v '^[<>-]' | wc -l | grep -o "[0-9]\+" )
 
     if [ "$chanchedLines" != "0" ] ; then
         # there are changes
-        rm locale/$lang/LC_MESSAGES/messages_old.po locale/$lang/LC_MESSAGES/messages_old_bak.po
-        msgfmt -o locale/$lang/LC_MESSAGES/messages.mo locale/$lang/LC_MESSAGES/messages.po
+        rm $localeDir/$lang/LC_MESSAGES/messages_old.po $localeDir/$lang/LC_MESSAGES/messages_old_bak.po
+        msgfmt -o $localeDir/$lang/LC_MESSAGES/messages.mo $localeDir/$lang/LC_MESSAGES/messages.po
     else
         # no changes -> keep old version
-        rm locale/$lang/LC_MESSAGES/messages_old.po
-        mv locale/$lang/LC_MESSAGES/messages_old_bak.po locale/$lang/LC_MESSAGES/messages.po
+        rm $localeDir/$lang/LC_MESSAGES/messages_old.po
+        mv $localeDir/$lang/LC_MESSAGES/messages_old_bak.po $localeDir/$lang/LC_MESSAGES/messages.po
     fi
 done
 
-rm $filesPHP $filesXML $potAll
-
+rm $filesPHP $filesXML $filesJS $potAll $potJS
