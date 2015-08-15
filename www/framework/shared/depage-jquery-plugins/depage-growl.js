605f1e2a8d24dd48e252d55816211512b0dd5ee5
Frank Hellenkamp <jonas@depage.net>
2015-08-15 17:06:52 +0200

    depage-growl: added basic support for system notifications with html fallback

--- www/framework/shared/depage-jquery-plugins/depage-growl.js
+++ www/framework/shared/depage-jquery-plugins/depage-growl.js
@@ -4,7 +4,7 @@
  * depage growl notification jquery-plugin
  *
  *
- * copyright (c) 2009-2011 Frank Hellenkamp [jonas@depage.net]
+ * copyright (c) 2009-2015 Frank Hellenkamp [jonas@depage.net]
  *
  * @todo use html-fallback and use system notifications based on new api
  * see here: https://github.com/alexgibson/notify.js/blob/master/notify.js
@@ -22,7 +22,59 @@
 
     var base = {};
 
-    base.growlFallback = function(title, message, icon) {
+    // {{{ htmlEncode
+    var htmlEncode = function(value) {
+        return $('<div/>').text(value).html();
+    };
+    // }}}
+
+    // {{{ growl()
+    base.growl = function(title, options) {
+        base.options = $.extend({}, base.defaultOptions, options);
+
+        if (base.testSystemSupport()) {
+            base.growlSystemNotification(title, base.options.message, base.options.icon);
+        } else {
+            base.growlHtmlFallback(title, base.options.message, base.options.icon);
+        }
+    };
+    // }}}
+    // {{{ testSystemSupport
+    base.testSystemSupport = function() {
+        var isSupported = 'Notification' in window;
+        var needsPermission = !(isSupported && Notification.permission === 'granted');
+        var permissionLevel = (isSupported ? Notification.permission : null);
+
+        console.log(isSupported);
+        console.log(Notification);
+
+        if (isSupported && needsPermission) {
+            Notification.requestPermission(function (perm) {
+                switch (perm) {
+                    case 'granted':
+                        console.log("granted");
+                        break;
+                    case 'denied':
+                        console.log("denied");
+                        break;
+                }
+            });
+        } else if (isSupported) {
+            return true;
+        }
+        return false;
+    };
+    // }}}
+    // {{{ growlSystemNotification
+    base.growlSystemNotification = function(title, message, icon) {
+        new Notification(title, {
+            body: message,
+            icon: icon
+        });
+    };
+    // }}}
+    // {{{ growlHtmlFallback
+    base.growlHtmlFallback = function(title, message, icon) {
         // get or add notification-area
         var $notificationArea = $("#depageGrowlArea");
 
@@ -35,11 +87,7 @@
             });
         }
 
-        title = title || "";
-        message = message || "";
-        icon = icon || "";
-
-        var $notification = $("<div><h3>" + title + "</h3><p>" + message + "</p></div>")
+        var $notification = $("<div><h3>" + htmlEncode(title) + "</h3><p>" + htmlEncode(message) + "</p></div>")
             .appendTo($notificationArea)
             .click( function() {
                 $(this).remove();
@@ -51,8 +99,20 @@
                 $(this).remove();
             });
     };
+    // }}}
 
-    // {{{ growl()
+    // {{{ defaultOptions
+    base.defaultOptions = {
+        message: "",
+        icon: "",
+        onShow: null,
+        onClose: null,
+        onClick: null,
+        onError: null
+    };
+    // }}}
+
+    // {{{ $.depage.growl()
     /**
         * @function growl()
         *
@@ -62,9 +122,8 @@
         * @param message   bodytext of the notification (optional)
         * @param icons     icon of the notification (optional)
         */
-    $.depage.growl = function(title, message, icon) {
-        console.log(title, message, icon);
-        base.growlFallback(title, message, icon);
+    $.depage.growl = function(title, options) {
+        base.growl(title, options);
     };
     // }}}
 })(jQuery);
