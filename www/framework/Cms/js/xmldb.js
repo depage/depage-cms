a7ac56b45bde7d7af011fd8da16cfc944312cb44
Frank Hellenkamp <jonas@depage.net>
2016-03-20 18:20:22 +0100

    added basic javascript library for xmldb

---
+++ www/framework/Cms/js/xmldb.js
@@ -0,0 +1,81 @@
+/*
+ * @require framework/shared/jquery-1.8.3.js
+ *
+ *
+ * @file    js/xmldb.js
+ *
+ * copyright (c) 2015 Frank Hellenkamp [jonas@depage.net]
+ *
+ * @author    Frank Hellenkamp [jonas@depage.net]
+ */
+
+var DepageXmldb = (function() {
+    "use strict";
+    /*jslint browser: true*/
+    /*global $:false */
+
+    function Xmldb(baseUrl, projectName, docName) {
+        this.projectName = projectName;
+        this.docName = docName;
+        this.baseUrl = baseUrl;
+        this.postUrl = baseUrl + "project/" + this.projectName + "/tree/" + this.docName;
+    }
+    Xmldb.prototype = {
+        // {{{ createNode()
+        createNode: function() {
+        },
+        // }}}
+        // {{{ copyNode()
+        copyNode: function() {
+        },
+        // }}}
+        // {{{ moveNode()
+        moveNode: function(nodeId, targetId, position) {
+            this.ajaxCall("moveNode", {
+                "id" : nodeId,
+                "target_id" : targetId,
+                "position" : position
+            });
+        },
+        // }}}
+        // {{{ renameNode()
+        renameNode: function() {
+        },
+        // }}}
+        // {{{ deleteNode()
+        deleteNode: function(nodeId) {
+            this.ajaxCall("deleteNode", {
+                "id" : nodeId
+            });
+        },
+        // }}}
+        // {{{ duplicateNode()
+        duplicateNode: function() {
+        },
+        // }}}
+
+        // {{{ ajaxCall()
+        ajaxCall: function(operation, data) {
+            $.ajax({
+                async: true,
+                type: 'POST',
+                url: this.postUrl + '/' +  operation + '/',
+                data: data,
+                dataType: 'json',
+                error: function(e) {
+                    console.log("error");
+                    console.log(e);
+                },
+                success: function(data, status) {
+                    console.log(status);
+                    console.log(data);
+                }
+            });
+        },
+        // }}}
+    };
+
+    return Xmldb;
+})();
+
+// vim:set ft=javascript sw=4 sts=4 fdm=marker :
