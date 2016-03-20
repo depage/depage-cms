782bfb47663be4f091082e56e1e1a8c0e5f61053
Frank Hellenkamp <jonas@depage.net>
2016-03-20 17:36:01 +0100

    updated camel-case renaming in html classes

--- Html.php
+++ Html.php
@@ -290,7 +290,7 @@ class Html {
         }
     }
     // }}}
-    // {{{ include_js_get_dependecies()
+    // {{{ includeJsGetDependencies()
     /**
      * gets dependencies from filename
      */
