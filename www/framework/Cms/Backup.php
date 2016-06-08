12a8f700f3574e3982c00dfd2ed31320b29ad669
Frank Hellenkamp <jonas@depage.net>
2016-06-08 18:00:10 +0200

    added stump for backing up project

---
+++ www/framework/Cms/Backup.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file    framework/Cms/Backup.php
+ *
+ * depage cms backup module
+ *
+ *
+ * copyright (c) 2016 Frank Hellenkamp [jonas@depage.net]
+ *
+ * @author    Frank Hellenkamp [jonas@depage.net]
+ */
+
+namespace Depage\Cms\Backup;
+
+/**
+ * brief Backup
+ * Class Backup
+ */
+class Backup
+{
+    // {{{ restore()
+    /**
+     * @brief restore
+     *
+     * @param mixed $
+     * @return void
+     **/
+    public static function restore($projectName, $file)
+    {
+
+    }
+    // }}}
+    // {{{ create()
+    /**
+     * @brief create
+     *
+     * @param mixed $
+     * @return void
+     **/
+    public static function create($projectName, $file)
+    {
+
+    }
+    // }}}
+}
+
+/* vim:set ft=php sw=4 sts=4 fdm=marker : */
