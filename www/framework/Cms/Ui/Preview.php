424e4ac9c00b39f79104914225091771cac484d7
Frank Hellenkamp <jonas@depage.net>
2018-01-16 12:46:40 +0100

    updated ui preview for new config class

--- www/framework/Cms/Ui/Preview.php
+++ www/framework/Cms/Ui/Preview.php
@@ -172,13 +172,9 @@ class Preview extends \Depage\Depage\Ui\Base
         $transformer = \Depage\Transformer\Transformer::factory($this->previewType, $xmlGetter, $this->projectName, $this->template, $transformCache);
         $transformer->routeHtmlThroughPhp = true;
 
-        $projectPath = $this->project->getProjectPath();
-        if (file_exists("$projectPath/lib/global/config.php")) {
-            include("$projectPath/lib/global/config.php");
-
-            if (isset($aliases)) {
-                $transformer->registerAliases($aliases);
-            }
+        $projectConfig = $this->project->getProjectConfig();
+        if (isset($projectConfig->aliases)) {
+            $transformer->registerAliases($projectConfig->aliases);
         }
 
         if ($this->template == "newsletter") {
