dc93b2cf0fa6e6d3e619d0c56611ae17a668a433
Frank Hellenkamp <jonas@depage.net>
2018-10-11 18:36:01 +0200

    udpated jstree to open node before creating child

--- www/framework/Cms/js/depage.jstree.js
+++ www/framework/Cms/js/depage.jstree.js
@@ -199,8 +199,8 @@
             } else if (typeof nextId !== 'undefined') {
                 xmldb.createNodeBefore(param.node.li_attr.rel, nextId, base.afterCreate, param.node.li_attr.xmlTemplateData);
             } else {
-                xmldb.createNodeIn(param.node.li_attr.rel, parentId, base.afterCreate, param.node.li_attr.xmlTemplateData);
                 jstree.open_node(parentId);
+                xmldb.createNodeIn(param.node.li_attr.rel, parentId, base.afterCreate, param.node.li_attr.xmlTemplateData);
             }
 
             jstree.disable_node(param.node);
