4fc945fae8c1e8cb4643d9869589dd3792bd9bff
Frank Hellenkamp <jonas@depage.net>
2018-11-03 00:03:05 +0100

    removed unused old test directory

--- tests/framework/graphics/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// use autoloader from depage class
-require_once('../../../www/framework/depage/depage.php');
-
-require_once('graphicsTestClass.php');
-require_once('graphics_imagemagickTestClass.php');
-require_once('graphics_graphicsmagickTestClass.php');
