5f41cf084e684123731b0f8d0665225a483472d1
Frank Hellenkamp <jonas@depage.net>
2021-01-21 16:05:30 +0100

    added small fixes for locking

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,4 +7,6 @@ require_once __DIR__ . '/graphics_imagemagickTestClass.php';
 require_once __DIR__ . '/graphics_graphicsmagickTestClass.php';
 require_once __DIR__ . '/graphics_procTestClass.php';
 
-@mkdir(__DIR__ . '/output');
+if (!is_dir(__DIR__ . '/output')) {
+    mkdir(__DIR__ . '/output');
+}
